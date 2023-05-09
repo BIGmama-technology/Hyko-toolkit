2e55ae61425c50a3f0216617c4ba5501b6e2974f
charaf-ceb <ceb@big-mama.io>
2023-05-09 18:20:24 +0100

    Finished SearchEngine

--- model_selection.py
+++ model_selection.py
@@ -2,14 +2,17 @@
 Utility functions for model-selection using semantic + beam search
 """
 
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
 import torch.nn as nn
 from torch.types import _TensorOrTensors
-from typing import Dict, List, Union
+import torch 
+from typing import Dict, List, Union, Tuple, Optional
 import numpy as np
 from functions.math.add.main import MetaData
 
-class Embdder(nn.Module):
+DEBUG = True
+
+class Embedder(nn.Module):
     r"""
     Embdder class: Holistic embbeding module for both model and tasks metadata databases.
     """
@@ -17,7 +20,6 @@ class Embdder(nn.Module):
 
     def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
         self.sent_bi_encoder = SentenceTransformer(model_name)
-        self.model_db = None
         if not model_name:
             raise ValueError("model_name must be specified")
 
@@ -91,15 +93,14 @@ class Embdder(nn.Module):
         output_tensor: bool = True,
         model_db: List[MetaData] = None,
         task_db: List[Dict] = None,
-    ):
+    ) -> Tuple[Union[List[np.ndarray], List[_TensorOrTensors]],
+               Union[List[np.ndarray], List[_TensorOrTensors]]]:
         r"""
         Embedder module forward function override.
         """
 
         if output_numpy == output_tensor:
             raise ValueError("Only one of output_numpy and output_tensor can be True")
-        else:
-            pass
 
         if not model_db:
             raise ValueError("Model DataBase must be specified")
@@ -114,18 +115,56 @@ class Embdder(nn.Module):
 
 class SemanticSearchEngine(nn.Module):
     def __init__(self):
-        pass
+        self.embedder = Embedder()
 
-    def similarity_measure(self):
+    def similarity_measure(self, model_db : List[MetaData],
+                                 task_db : List[Dict],
+                                 top_k : int = 5):
+        
         r"""
         similarity_measure : measures Cross Semantic Textual Similarity between model_db_embeds
                              and task_db_embeds.
                              args: model_db_embeds : List[Union[np.ndarray, torch.Tensor]]
                                    task_db_embeds : List[Union[np.ndarray, torch.Tensor]
 
-                             outputs: Cross STS (Semantic Textual Similarity) Matrix (Union[torch.Tensor, np.ndarray])
-        
-        NOTE : Can use Semantic-Search or Elastic-Search from sentence-transformers
+                             outputs: List of node_candidates at each task (task-planner output item)
         """
+
+        # embed model and task dbases
+        model_db_embeds, task_db_embeds = self.embedder(model_db = model_db,
+                                                        task_db = task_db)
+    
+        # Iterate over model_base_embed and apply semantic search for each Query(task_embed)
+        top_k = min(top_k, len(model_db_embeds))
+
+        node_wise_cand_idx = [] # list for model candidates at each sequential task
+        for task_embed in task_db_embeds:
+
+            description_sim_scores = util.cos_sim(task_embed['description_embed'], [model_embed["description_embed"] for model_embed in model_db_embeds])[0]
+            input_descr_sim_scores = util.cos_sim(task_embed["input_descr"], [model_embed["input_descr"] for model_embed in model_db_embeds])[0]
+            output_descr_sim_scores = util.cos_sim(task_embed["output_descr"], [model_embed["output_input"] for model_embed in model_db_embeds])[0]
+            
+            # score weighted average
+            avg_tens = (6*description_sim_scores) + (2*input_descr_sim_scores) + (2*output_descr_sim_scores)
+            avg_tens /= 3
+            
+            candidates = torch.topk(avg_tens, k = top_k)[1] # [1] for idx
+            node_wise_cand_idx.append(candidates)
+            if DEBUG:
+                print(type(candidates)) # Debugging
+
+        return node_wise_cand_idx
+    
+    def forward(self, model_db : List[MetaData], task_db : List[Dict]) -> List[int]:
         
-        pass
+        model_id_list : List[Optional[int]] = [MetaData_.id for MetaData_ in model_db]
+
+        node_wise_cand_idx = self.similarity_measure(model_db, task_db)
+
+        node_wise_cand_ids = []
+
+        for idx_list in node_wise_cand_idx:
+            cand_ids = np.array(node_wise_cand_idx)[idx_list]
+            node_wise_cand_ids.append(cand_ids.tolist())
+
+        return node_wise_cand_ids
