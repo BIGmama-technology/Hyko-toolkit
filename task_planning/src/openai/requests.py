96d391aa6d55382b2516772d5aab40c5b20debfd
hachem <hb@big-mama.io>
2023-05-09 16:11:30 +0100

    add task planning

--- task_planning/src/openai/requests.py
+++ task_planning/src/openai/requests.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from dotenv import load_dotenv
 
@@ -10,35 +11,72 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def chatgpt_completion(conversation: str) -> str:
-    conversation = [{"role": "user", "content": conversation}]
+def chatgpt_completion(conversation: list, max_tokens: int=100, logit_bias={}) -> str:
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-0301",
         messages=conversation,
+        n=1,
+        max_tokens=max_tokens,
+        logit_bias=logit_bias,
     )
     return completion.choices[0].message.content
 
 
-def task_planning(prompt: str):
+def input_description(prompt: str) -> str:
+    """
+    max_tokens: 200
+    n : 1
+    """
+    conversation = [
+        {
+            "role": "system",
+            "content": "from a description of an AI model give a description of the inputs",
+        },
+        {"role": "user", "content": prompt},
+    ]
+    completion = chatgpt_completion(conversation)
+    return completion
+
+
+def output_description(prompt: str) -> str:
+    """
+    max_tokens: 200
+    n : 1
+    """
+    conversation = [
+        {
+            "role": "system",
+            "content": "from a description of an AI model give a description of the outputs",
+        },
+        {"role": "user", "content": prompt},
+    ]
+    completion = chatgpt_completion(conversation)
+    return completion
+
+
+def few_shot_task_planning(prompt: str):
     conversation = [{"role": "system", "content": TASK_PLANNING_PROMPT}]
     conversation += FEW_SHOT_TASK_PLANNING
     conversation += [{"role": "user", "content": prompt}]
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=conversation,
-    )
+    completion = chatgpt_completion(conversation)
+
     # task_planning_result = eval(completion.choices[0].message.content)
     # return task_planning_result
-    return completion.choices[0].message.content
+    return completion
 
+def zero_shot_task_planning(subject: str, logit_bias={58:100}):
+    conversation = [{"role": "system", "content": TASK_PLANNING_PROMPT}]
+    conversation += [{"role": "user", "content": "give a task plan (a json between two ```) for this subject : " + subject}]
+    completion = chatgpt_completion(conversation, max_tokens=1000)
 
-def model_classification(task: str, meta: str) -> str:
-    conversation = [
-        {"role": "system", "content": MODEL_CLASSIFICATION_PROMPT(meta, task)}
-    ]
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=conversation,
-    )
-    choice_id = eval(completion.choices[0].message.content)["id"]
-    return choice_id
+    result = re.search(r'```json(.+?)```', completion, flags=re.DOTALL)
+    if result:
+        extracted_text = result.group(1)
+        return extracted_text
+    
+    result = re.search(r'```(.+?)```', completion, flags=re.DOTALL)
+    if result:
+        extracted_text = result.group(1)
+        return extracted_text
+    
+    raise Exception("error parsing output of zero_shot_task_planning")
--- task_planning/src/prompts/prompts.py
+++ task_planning/src/prompts/prompts.py
@@ -2,12 +2,12 @@ ARGS = """["text", "image", "audio", "pdf"]"""
 
 TASK_FORMAT = """
 [{
-"task_id":id, 
+"task_id":unique_task_id, 
 "task": task_name, 
 "task_description": task_description, 
 "dep": [dependency_task_id],
-"inputs": [{"input_id":id, "input_type":type, "input_description":desc}]
-"outputs": [{"output_id":generated_id, "output_type":type, "output_description":desc}] 
+"inputs": [{"id":generated_unique_id, "input_type":type, "input_description":desc}]
+"outputs": [{"id":generated_unique_id, "output_type":type, "output_description":desc}] 
 }]
 """
 
@@ -26,11 +26,13 @@ AVAILABLE_TASKS = """
 "document-question-answering", "similarity_measure"
 """
 
-TASK_PLANNING_PROMPT = f"""Task Planning Stage: The AI assistant can parse user input to several tasks: {TASK_FORMAT}
-The "dep" field denotes the ids of the previous prerequisite tasks which generate a new resource that the current task relies on. 
-The input_type and output_type fields must be in {ARGS}, nothing else. The task 
-must be selected from the following options: {AVAILABLE_TASKS}.
-There may be multiple tasks of the same type. 
-Parse out as few tasks as possible while ensuring that the user request can be resolved. 
-Pay attention to the dependencies and order among tasks. 
+TASK_PLANNING_PROMPT = f"""Task Planning Stage: The AI assistant can suggest the tasks needed 
+to achieve from user objective, following this format: {TASK_FORMAT}
+The "dep" field denotes the ids of the previous prerequisite tasks which generate needed inputs 
+that the current task relies on. 
+The input_type and output_type fields must be in {ARGS}, nothing else. 
+example of available tasks : {AVAILABLE_TASKS}.
+Use as few tasks as possible while ensuring that the user request can be resolved. 
+Pay attention to the dependencies and order among tasks.
+Make sure of the consistency of the input of a task and the output of it previous task using the generated unique ids.
 """
