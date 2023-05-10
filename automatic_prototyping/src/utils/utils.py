a373a8fae79bdf89c652d44893d51a01b35ecfaa
hachem <hb@big-mama.io>
2023-05-10 15:44:42 +0100

    add readjson utils

--- automatic_prototyping/src/utils/utils.py
+++ automatic_prototyping/src/utils/utils.py
@@ -6,6 +6,11 @@ def save_json(filename, json_data):
     with open(filename, "w") as f:
         json.dump(json_data, f)
 
+def read_json(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data
+
 class MetaData(BaseModel):
     name: str
     description: str
