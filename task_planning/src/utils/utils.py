facc1c8405be401e8ea4632b5633b032e6c843fd
hachem <hb@big-mama.io>
2023-05-10 14:36:07 +0100

    include model selection

--- task_planning/src/utils/utils.py
+++ task_planning/src/utils/utils.py
@@ -1,6 +1,18 @@
 import json
-
+from pydantic import BaseModel
+from typing import List
 
 def save_json(filename, json_data):
     with open(filename, "w") as f:
         json.dump(json_data, f)
+
+class MetaData(BaseModel):
+    name: str
+    description: str
+    version: str
+    category: str
+    inputs: List[str]
+    inputs_descr: str
+    outputs: List[str]
+    outputs_descr: str
+    params: List[str]
