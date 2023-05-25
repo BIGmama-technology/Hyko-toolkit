5ef640803d3f4bd461b7e7ccda24a1c0544c8ca3
Lotfi Lehamel <lotfi.lehamel@outlook.com>
2023-05-25 15:16:09 +0100

    feat: added extra field for ports + refactor port types

--- build-push-all.py
+++ build-push-all.py
@@ -4,7 +4,7 @@ from sdk.common.metadata import MetaData
 import json
 
 
-def metadata_to_labels(meta: MetaData) -> dict[str, str]:
+def metadata_to_labels(meta: MetaData) -> dict[str, float | int | str | bool]:
     labels = {}
 
     labels["HYKO_SDK_NAME"] = meta.name
@@ -16,21 +16,36 @@ def metadata_to_labels(meta: MetaData) -> dict[str, str]:
 
     for inp in meta.inputs:
         labels[f"HYKO_SDK_INPUT_{i}_NAME"] = inp.name
+        if inp.description is not None:
+            labels[f"HYKO_SDK_INPUT_{i}_DESC"] = inp.description
         labels[f"HYKO_SDK_INPUT_{i}_TYPE"] = inp.type
+        labels[f"HYKO_SDK_INPUT_{i}_REQUIRED"] = inp.required
+        if inp.default is not None:
+            labels[f"HYKO_SDK_INPUT_{i}_DEFAULT"] = inp.default
         i += 1
 
     i = 0
 
     for param in meta.params:
         labels[f"HYKO_SDK_PARAM_{i}_NAME"] = param.name
+        if param.description is not None:
+            labels[f"HYKO_SDK_PARAM_{i}_DESC"] = param.description
         labels[f"HYKO_SDK_PARAM_{i}_TYPE"] = param.type
+        labels[f"HYKO_SDK_PARAM_{i}_REQUIRED"] = param.required
+        if param.default is not None:
+            labels[f"HYKO_SDK_PARAM_{i}_DEFAULT"] = param.default
         i += 1
 
     i = 0
 
     for out in meta.outputs:
         labels[f"HYKO_SDK_OUTPUT_{i}_NAME"] = out.name
+        if out.description is not None:
+            labels[f"HYKO_SDK_OUTPUT_{i}_DESC"] = out.description
         labels[f"HYKO_SDK_OUTPUT_{i}_TYPE"] = out.type
+        labels[f"HYKO_SDK_OUTPUT_{i}_REQUIRED"] = out.required
+        if out.default is not None:
+            labels[f"HYKO_SDK_OUTPUT_{i}_DEFAULT"] = out.default
         i += 1
 
     return labels
