21355443438e65f64c834e20295a227d4896d653
charaf-ceb <ceb@big-mama.io>
2023-06-06 09:20:06 +0100

    Const TASk Planning

--- automatic_prototyping/src/const_task_planning/model_db_parser.py
+++ automatic_prototyping/src/const_task_planning/model_db_parser.py
@@ -4,14 +4,13 @@ import json
 model_data_path = Path("../../data/")
 
 model_metadata = []
-with open(model_data_path /  "huggingface_models.jsonl", "r") as db_file:
+with open(model_data_path / "huggingface_models.jsonl", "r") as db_file:
     for model in db_file:
-        
         model = json.loads(model)
-        model_metadata.append({str(model['id']) : model['description']})
+        model_metadata.append({str(model["id"]): model["description"]})
 
 # print(model_metadata[0])
-model_metadata = {k:v for model in model_metadata for k,v in model.items()}
+model_metadata = {k: v for model in model_metadata for k, v in model.items()}
 model_metadata = {"models": model_metadata}
 
 
