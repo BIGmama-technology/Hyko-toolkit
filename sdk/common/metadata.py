fb13c9bba91edb4dc475f1510331ff03f6b63e6a
Lotfi Lehamel <lotfi.lehamel@outlook.com>
2023-06-18 12:25:44 +0100

    feat: added image and audio types

--- sdk/common/metadata.py
+++ sdk/common/metadata.py
@@ -14,16 +14,26 @@ def pmodel_to_ports(pmodel: BaseModel) -> list[IOPort]:
     
     for field_name, field_props in fields_properties.items():
         field_type = field_props.get("type")
-        
+
         if field_type is None:
             continue
-        
+
         if field_type == "number":
             port_type = IOPortType.NUMBER
         elif field_type == "integer":
             port_type = IOPortType.INTEGER
         elif field_type == "string":
-            port_type = IOPortType.STRING
+            string_format = field_props.get("format")
+
+            if string_format is not None:
+                if string_format == "image":
+                    port_type = IOPortType.IMAGE
+                elif string_format == "audio":
+                    port_type = IOPortType.AUDIO
+                else:
+                    port_type = IOPortType.STRING
+            else:
+                port_type = IOPortType.STRING
         else:
             continue
 
