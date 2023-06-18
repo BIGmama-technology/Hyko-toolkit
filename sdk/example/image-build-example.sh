9c69c2ac8e13d2ad79066134921e9cb7e8554433
Lotfi Lehamel <lotfi.lehamel@outlook.com>
2023-06-18 12:25:26 +0100

    refactor: removed obsolete example

--- sdk/example/image-build-example.sh
+++
@@ -1,7 +0,0 @@
-set -x
-set -e
-
-# dont use this directly, make sure to change the name and versioning correctly
-docker login registry.gitlab.com
-docker build -t registry.gitlab.com/big-mama1/big-mama-tech/hyko/backend-services/ai-sdk/add:1.0 .
-docker push registry.gitlab.com/big-mama1/big-mama-tech/hyko/backend-services/ai-sdk/add:1.0
