bf0e605e953252b07a285def66b3da7ccf500319
Lotfi Lehamel <lotfi.lehamel@outlook.com>
2023-06-18 12:25:59 +0100

    feat: added sting concatenation function

---
+++ sdk/utils-string/concat/main.py
@@ -0,0 +1,20 @@
+import fastapi
+from .config import Inputs, Params, Outputs
+
+app = fastapi.FastAPI()
+
+#################################################################
+
+# Insert the main code of the function here #################################################################
+
+
+# keep the decorator, function declaration and return type the same.
+# the main function should always take Inputs as the first argument and Params as the second argument.
+# should always return Outputs.
+@app.post("/", response_model=Outputs)
+async def main(inputs: Inputs, params: Params):
+    return Outputs(output=(inputs.first + inputs.second))
+
+
+##############################################################################################################
+
