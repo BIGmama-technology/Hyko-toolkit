236b6cfdce4ee02c6c6933427a26fd863ce0e99f
Lotfi Lehamel <lotfi.lehamel@outlook.com>
2023-06-19 17:19:25 +0100

    feat: added openai text-davinci-003

--- sdk/OpenAI/text-davinci-003/main.py
+++ sdk/OpenAI/text-davinci-003/main.py
@@ -1,7 +1,6 @@
 import fastapi
-from fastapi.responses import JSONResponse
 from .config import Inputs, Params, Outputs
-import httpx
+import openai
 
 app = fastapi.FastAPI()
 
@@ -15,37 +14,17 @@ app = fastapi.FastAPI()
 # should always return Outputs.
 @app.post("/", response_model=Outputs)
 async def main(inputs: Inputs, params: Params):
-    
-    async with httpx.AsyncClient() as client:
-        res = await client.post(
-            url="https://api.openai.com/v1/completions",
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {inputs.api_key}",
-            },
-            json={
-                "model": "text-davinci-003",
-                "prompt": inputs.prompt,
-                "max_tokens": int(inputs.max_tokens),
-                "temperature": inputs.temperature,
-                "top_p": int(inputs.top_p),
-            }
-        )
 
-        if res.is_success:
-            try:
-                generated_text = res.json()["choices"][0]["text"]
-                return Outputs(generated_text=generated_text)
-            except:
-                return JSONResponse(
-                    status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    content="Error while decoding OpenAi API response",
-                )
-            
-        return JSONResponse(
-            status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content=f"OpenAi API error ({res.status_code}): {res.text}",
-        )
+    res = await openai.Completion.acreate(
+        model="text-davinci-003",
+        prompt=inputs.prompt,
+        api_key=inputs.api_key,
+        max_tokens=inputs.max_tokens,
+        temperature=inputs.temperature,
+        top_p=inputs.top_p,
+    )
+
+    return Outputs(generated_text=res.get("choices")[0]["text"]) # type: ignore
 
 
 ##############################################################################################################
