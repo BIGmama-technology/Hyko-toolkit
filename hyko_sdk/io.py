2395b767c4e0c1a14dc196a6668e237968c459ef
amd_dahmadjid <amd2@big-mama.io>
2023-06-26 12:45:33 +0100

    feat: added image conversions to/from base64 and audio decoding from webm to wav

--- hyko_sdk/io.py
+++ hyko_sdk/io.py
@@ -1,3 +1,4 @@
+from .error import BaseError, Errors
 from enum import Enum
 from typing import Optional
 from pydantic import BaseModel
@@ -8,6 +9,10 @@ from pydantic import BaseModel
 from pydantic.fields import ModelField
 
 from pydantic import BaseModel
+import numpy as np
+import cv2
+import base64
+import ffmpeg
 
 
 class Number(float):
@@ -69,7 +74,6 @@ class String(str):
 
 
 class Image(str):
-
     @classmethod
     def __get_validators__(cls) -> Generator[Callable, None, None]:
         yield cls.validate
@@ -88,6 +92,27 @@ class Image(str):
             field_schema["type"] = "string"
             field_schema["format"] = "image"
 
+    def decode(self) -> tuple[np.ndarray | None, BaseError | None]:
+        if len(self.split(",")) != 2:
+            return None, Errors.InvalidBase64
+        
+        header, data = self.split(",")
+        if not ("image" in header):
+            return None, Errors.Base64NotAnImage
+        base64_bytes = base64.b64decode(data)
+        np_bytes = np.frombuffer(base64_bytes, np.uint8)
+        img = cv2.imdecode(np_bytes, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img, None
+    
+
+class AudioMetadata(BaseModel):
+    sample_rate: int
+    duration: float
+    bit_rate: int
+    channels: int
+    bits_per_sample: int
+    bit_rate: int
 
 class Audio(str):
 
@@ -109,7 +134,22 @@ class Audio(str):
             field_schema["type"] = "string"
             field_schema["format"] = "audio"
 
+    def decode(self) -> tuple[np.ndarray | None, AudioMetadata | None, BaseError | None]:
+        if len(self.split(",")) != 2:
+            return None, None ,Errors.InvalidBase64
+        
+        header, data = self.split(",")
+        if not ("audio" in header):
+            return None, None, Errors.Base64NotAnAudio
+        base64_bytes = base64.b64decode(data)
+        with open("audio.webm", "wb") as f:
+            f.write(base64_bytes)
+        ffmpeg.input("audio.webm").output("audio.wav").run()
 
+        meta_data: dict = ffmpeg.probe("audio.wav")
+        with open("audio.wav", "rb") as f:
+            audio = f.read()
+        return np.frombuffer(audio, dtype=np.float64), AudioMetadata(**meta_data), None
 # Keep the same
 class IOPortType(str, Enum):
     NUMBER = "number"
@@ -129,3 +169,14 @@ class IOPort(BaseModel):
     type: IOPortType
     required: bool
     default: Optional[float | int | str]
+
+
+def image_to_base64(image: np.ndarray) -> tuple[Image, BaseError | None]:
+    """
+    Takes an RGB pixel data as a numpy array and compress it to png and encode it as base64
+    """
+    ret, image = cv2.imencode(".png", image)
+    
+    data = base64.b64encode(image.tobytes())
+    img = Image("data:image/png;base64," + data.decode())
+    return img, None
