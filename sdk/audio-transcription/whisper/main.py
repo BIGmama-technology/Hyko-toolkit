840dc531cbd0f2d5ffa79fb52b18839eed1c9c33
charaf-ceb <ceb@big-mama.io>
2023-06-26 15:32:43 +0100

    torchaudio

--- sdk/audio-transcription/whisper/main.py
+++ sdk/audio-transcription/whisper/main.py
@@ -1,35 +1,37 @@
-from config import Inputs, Outputs, Params
+from config import Inputs, Outputs, Params, Audio
 import fastapi
 from transformers import WhisperProcessor, WhisperForConditionalGeneration, audio_utils
-import numpy as np
 import torch
-import base64
 import torchaudio
-
-# import ffmpeg
-import io
-import soundfile as sf
 
 app = fastapi.FastAPI()
 
-processor = WhisperProcessor.from_pretrained("openai/whisper-small")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
+processor = WhisperProcessor.from_pretrained("openai/whisper-large-v2")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v2").cuda()
 model.config.forced_decoder_ids = None
+
 
 @app.post("/", response_model=Outputs)
 async def main(inputs: Inputs, params: Params):
-    audio_bytes = base64.urlsafe_b64decode(inputs.input_audio.split(",")[1])
-    print(type(inputs.input_audio))
-    input_audio_arr, metadata, err_  = inputs.input_audio.decode() 
+    _, metadata, err_ = Audio(inputs.input_audio).decode(sampling_rate = 16_000)
     if err_:
-     raise fastapi.HTTPException(status_code=500, details=err_.json())
-    # print(data, "\n", sample_rate)
-    #  = np.frombuffer(audio_bytes, dtype=np.float64)
-    # print("Numpy Audio: ", audio_)
-    print(input_audio_arr, "\n", metadata)
-    # audio_ = torch.from_numpy(data)
-    print("Numpy Audio Shape: ", audio_.shape)
-    input_features = processor.feature_extractor(audio_, sampling_rate = 16_000, return_tensors="pt")
-    print("Input Features: ", input_features)
-    # return Outputs(output_text=None)
-    return Outputs(output_text = str(input_features))
+        raise fastapi.HTTPException(status_code=500, detail=err_.json())
+    
+    waveform, sample_rate = torchaudio.load("audio.wav")
+    waveform = waveform.numpy()[0]
+
+    input_features = processor.feature_extractor(
+        waveform, sampling_rate=16_000, return_tensors="pt"
+    ).input_features 
+    
+    # print(metadata.streams[0].sample_rate)
+
+    with torch.no_grad():
+        output_toks = model.generate(input_features.cuda())
+        print(output_toks)
+        transcription = processor.batch_decode(
+            output_toks, max_new_tokens=10000, skip_special_tokens=True
+        )
+        print(transcription)
+
+    return Outputs(output_text=str(transcription))
