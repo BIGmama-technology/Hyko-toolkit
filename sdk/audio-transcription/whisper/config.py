b233de54fa4811b7194700f313e78c7468e860f5
charaf-ceb <ceb@big-mama.io>
2023-06-26 13:59:02 +0100

    feat: added whisper (offline)

---
+++ sdk/audio-transcription/whisper/config.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from hyko_sdk.metadata import MetaData, pmodel_to_ports
+from hyko_sdk.io import String, Audio
+from typing import List
+
+# Metadata
+
+name = "Whisper"
+description = "OpenAI's Whisper"
+version = "1.0"
+category = "Speech recognition"
+task = "Speech to text translation"
+
+
+class Inputs(BaseModel):
+    input_audio: Audio
+
+
+# Parameters to the function like temperature for gpt3. These values are constant  n runtime
+class Params(BaseModel):
+    pass
+
+
+class Outputs(BaseModel):
+    output_text: str
+
+
+# Function metadata, should always be here
+
+__meta_data__ = MetaData(
+    name=name,
+    description=description,
+    version=version,
+    category=category,
+    task=task,
+    inputs=pmodel_to_ports(Inputs),  # type: ignore
+    params=pmodel_to_ports(Params),  # type: ignore
+    outputs=pmodel_to_ports(Outputs),  # type: ignore
+)
