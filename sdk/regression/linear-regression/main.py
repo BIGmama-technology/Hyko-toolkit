1a5aa5adb0ad06dcad007b2442959499475ca1c1
charaf-ceb <ceb@big-mama.io>
2023-07-02 12:59:17 +0100

    Added Linear regression

---
+++ sdk/regression/linear-regression/main.py
@@ -0,0 +1,21 @@
+from config import Inputs, Outputs, Params, Audio
+import fastapi
+from typing import Union
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+linear_regression = LinearRegression()
+
+app = fastapi.FastAPI()
+
+@app.post("/", response_model=Outputs)
+async def main(inputs: Inputs, params: Params):
+    assert len(params.input_x) == len(params.input_y), "X and Y lists are of different Lengths"
+    
+    X = np.array(params.input_x).reshape(-1,1)
+    Y = np.array(params.input_y).reshape(-1,1)
+    
+    linear_regression.fit(X, Y)
+    output = linear_regression.predict([[inputs.input_sample]])
+
+    return Outputs(prediction=output)
