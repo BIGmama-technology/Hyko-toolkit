b238e9910a074f0070063d93396069d2146c772f
amd_dahmadjid <amd2@big-mama.io>
2023-07-03 11:12:41 +0100

    fixed bugs with audio and image loading

--- sdk/audio-transcription/whisper/main.py
+++ sdk/audio-transcription/whisper/main.py
@@ -13,21 +13,21 @@ model.config.forced_decoder_ids = None
 @app.post("/", response_model=Outputs)
 async def main(inputs: Inputs, params: Params):
     waveform, sample_rate, err_ = Audio(inputs.input_audio).decode(sampling_rate = 16_000)
-    if err_:
+    if err_ :
         raise fastapi.HTTPException(status_code=500, detail=err_.json())
+    if waveform is None or sample_rate is None:
+        raise fastapi.HTTPException(status_code=500, detail="Unexpected Error")
     
     input_features = processor.feature_extractor(
-        waveform, sampling_rate=16_000, return_tensors="pt"
+        waveform[0], sampling_rate=16_000, return_tensors="pt"
     ).input_features 
     
     # print(metadata.streams[0].sample_rate)
 
     with torch.no_grad():
         output_toks = model.generate(input_features.cuda())
-        print(output_toks)
         transcription = processor.batch_decode(
             output_toks, max_new_tokens=10000, skip_special_tokens=True
         )
-        print(transcription)
 
     return Outputs(output_text=str(transcription))
