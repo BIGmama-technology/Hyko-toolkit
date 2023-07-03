3f63c3edd51ee3634b82bc9dac760cdf64cba167
amd_dahmadjid <amd2@big-mama.io>
2023-07-03 10:14:42 +0100

    removed opencv and torchaudio

--- hyko_sdk/io.py
+++ hyko_sdk/io.py
@@ -6,11 +6,12 @@ from pydantic import BaseModel
 from pydantic.fields import ModelField
 
 import numpy as np
-import cv2
+from PIL import Image as PIL_Image
 import base64
-import torchaudio
+import soundfile
 import os
 import subprocess
+import io
 
 class Boolean(int):
     @classmethod    
@@ -115,11 +116,21 @@ class Image(str):
         if not ("image" in header):
             return None, Errors.Base64NotAnImage
         base64_bytes = base64.b64decode(data)
-        np_bytes = np.frombuffer(base64_bytes, np.uint8)
-        img = cv2.imdecode(np_bytes, cv2.IMREAD_COLOR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return img, None
+        img_bytes_io = io.BytesIO(base64_bytes)
+        img = PIL_Image.open(img_bytes_io)
+        return np.asarray(img), None
     
+
+
+_SUBTYPE2DTYPE = {
+    "PCM_S8": "int8",
+    "PCM_U8": "uint8",
+    "PCM_16": "int16",
+    "PCM_32": "int32",
+    "FLOAT": "float32",
+    "DOUBLE": "float64",
+}
+
 class Audio(str):
 
     @classmethod
@@ -140,12 +151,16 @@ class Audio(str):
             field_schema["type"] = "string"
             field_schema["format"] = "audio"
 
-    def decode(self, sampling_rate: Optional[int]  = None) -> Tuple[Optional[np.ndarray], Optional[int], Optional[BaseError]]:
+    def decode(self, sampling_rate: Optional[int] = None, 
+            normalize: bool = True,     
+            frame_offset: int = 0,
+            num_frames: int = -1,) -> Tuple[Optional[np.ndarray], Optional[int], Optional[BaseError]]:
+        
         if len(self.split(",")) != 2:
             return None, None ,Errors.InvalidBase64
         
         header, data = self.split(",")
-        if not ("audio" in header) or not ("webm" in header):
+        if not ("audio" in header) and not ("webm" in header):
             return None, None, Errors.Base64NotAnAudio
         base64_bytes = base64.b64decode(data)
         with open("audio.webm", "wb") as f:
@@ -157,9 +172,18 @@ class Audio(str):
         else:
             subprocess.run(f"ffmpeg -i audio.webm -o audio.wav".split(" "))
         
+        with soundfile.SoundFile("audio.wav", "r") as file_:
+            if file_.format != "WAV" or normalize:
+                dtype = "float32"
+            elif file_.subtype not in _SUBTYPE2DTYPE:
+                raise ValueError(f"Unsupported subtype: {file_.subtype}")
+            else:
+                dtype = _SUBTYPE2DTYPE[file_.subtype]
 
-        waveform, sample_rate = torchaudio.load("audio.wav")
-        return waveform.numpy()[0], sample_rate, None
+            frames = file_._prepare_read(frame_offset, None, num_frames)
+            waveform: np.ndarray = file_.read(frames, dtype, always_2d=True)
+            sample_rate: int = file_.samplerate
+            return waveform, sample_rate, None
     
     
 # Keep the same
