dae6cac2436e2098f1951c3bc77ef9bf0dadb095
charaf <ceb@big-mama.io>
2023-07-08 21:34:55 +0000

    added bounding boxes

--- sdk/visual-recognition/owlvit-base-patch32/main.py
+++ sdk/visual-recognition/owlvit-base-patch32/main.py
@@ -4,8 +4,11 @@ from config import Inputs, Params, Outputs
 from transformers import OwlViTProcessor, OwlViTForObjectDetection
 from hyko_sdk.io import image_to_base64
 import torch
+from PIL import Image, ImageDraw
+import numpy as np
+
 processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
-model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
+model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32").cuda()
 
 app = fastapi.FastAPI()
 
@@ -20,24 +23,42 @@ app = fastapi.FastAPI()
 
 @app.post("/", response_model=Outputs)
 async def main(inputs: Inputs, params: Params):
-    img = inputs.img.decode()
 
-    inputs = processor(text=[inputs.tags], images = torch.from_numpy(img), return_tensors='pt')
-    # print(inputs)
+    img = Image.fromarray(inputs.img.decode()[0][..., :3]).convert("RGB")
+    # img.save("TEST.jpg")
+    draw = ImageDraw.Draw(img)
+    texts = inputs.tags
+
+    inputs = processor(text=[texts], images = torch.from_numpy(np.array(img)).cuda(), return_tensors='pt').to("cuda")
+
     with torch.no_grad():
         outputs = model(**inputs)
-    # print(img.shape)
-    # target image size
-    target_sizes = torch.Tensor([torch.from_numpy(img).size()[::-1]]).to(torch.int32).squeeze()[1:]
     
-    print("Target Sizes: ", target_sizes)
-    print("Logits", outputs.logits.shape)
-    print("boxes", outputs.pred_boxes.shape)
+    target_sizes = torch.Tensor([torch.from_numpy(np.asarray(img)).size()[::-1]]).to(torch.int32).squeeze()[1:].to("cuda")
+    
+    # print("Target Sizes: ", target_sizes)
+    # print("Logits", outputs.logits.shape)
+    # print("boxes", outputs.pred_boxes.shape)
 
     results = processor.post_process(outputs=outputs, target_sizes=torch.stack([target_sizes]))
     # text = inputs.tags
     boxes, scores, labels = results[0]["boxes"], results[0]["scores"], results[0]["labels"]
-    # print(boxes.shape,"\n",scores.shape, "\n", labels.shape)
-    
-    print("Len Results", len(results))
-    return Outputs(bbox_tag_dict=None)
+    # print(boxes,"\n", scores, "\n", labels)
+    score_threshold = 0.1
+    detections = []
+ 
+    for box, score, label in zip(boxes, scores, labels):
+        box = [round(i, 2) for i in box.tolist()]
+        if score >= score_threshold:
+
+            detections.append({"label" : texts[label.item()],
+                               "confidence" : round(score.item(), 3),
+                               "location" : box})
+
+    for detection in detections:
+
+        xmin, ymin, xmax, ymax = detection["location"]
+        draw.rectangle((xmin+80, ymin, xmax+80, ymax), outline="red", width=1)
+        draw.text(detection["location"][:2], f"{detection['label']}: {detection['confidence']}", fill="red")
+
+    return Outputs(output_image=image_to_base64(np.asarray(img)))
