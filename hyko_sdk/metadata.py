ec9a641032996fa0f70de301c23da2be65c7b979
amd_dahmadjid <amd2@big-mama.io>
2023-08-13 14:33:47 +0100

    feat: added enum support

--- hyko_sdk/metadata.py
+++ hyko_sdk/metadata.py
@@ -1,5 +1,5 @@
 from typing import Any, List, Optional, Dict
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from enum import Enum
 
 class IOPortType(str, Enum):
@@ -16,6 +16,11 @@ class HykoExtraTypes(str, Enum):
     AUDIO = "audio"
     VIDEO = "video"
     
+    
+class JsonSchemaEnum(BaseModel):
+    enum: list[str]
+    type: str
+    
 class Property(BaseModel):
     type: Optional[IOPortType | HykoExtraTypes] = None
     anyOf: Optional [List['Property']] = None
@@ -25,10 +30,12 @@ class Property(BaseModel):
     maxItems: Optional[int] = None
     description: Optional[str] = None
     default: Optional[Any] = None
-
+    ref: Optional[str] = Field(default=None, alias="$ref") 
+    
 class HykoJsonSchema(BaseModel):
     properties: Dict[str, Property] = {}
     required: List[str] = []
+    defs: Optional[Dict[str, JsonSchemaEnum]] = Field(default=None, alias="$defs")
     
 class HykoJsonSchemaExt(HykoJsonSchema):
     friendly_property_types: Dict[str, str] = {}
