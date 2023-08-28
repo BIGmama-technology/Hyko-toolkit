04f1f0a8e4313aaed83e1195f354f42233ed531a
big-mama <ramy_h08@pop-os.localdomain>
2023-08-28 09:54:26 +0100

    feat: migrated concat and split to new sdk

--- sdk/utils/string/split/v1/main.py
+++ sdk/utils/string/split/v1/main.py
@@ -1,22 +1,25 @@
-from fastapi import FastAPI
-from config import Inputs, Params, Outputs
+from pydantic import Field
+from hyko_sdk import CoreModel, SDKFunction
 
-app = FastAPI()
 
-@app.post(
-    "/load",
-    response_model=None,
+func = SDKFunction(
+    description="Split a string to a list of strings based on delimiter",
+    requires_gpu=False,
 )
-def load():
-    pass
-
-@app.post(
-    "/",
-    response_model=Outputs,
-)
-async def main(inputs: Inputs, params: Params):
-    return Outputs(splitted=inputs.text.split(params.delimeter))
 
 
-##############################################################################################################
+class Inputs(CoreModel):
+    text: str = Field(..., description="Input text")
 
+class Params(CoreModel):
+    delimeter: str = Field(default=',', description="the string used to split the text by")
+
+class Outputs(CoreModel):
+    splitted: list[str]  = Field(..., description="List of strings that resulted from splitting by the delimeter")
+
+
+@func.on_execute
+async def main(inputs: Inputs , params: Params)-> Outputs:
+   return Outputs(splitted=inputs.text.split(params.delimeter))
+
+   
