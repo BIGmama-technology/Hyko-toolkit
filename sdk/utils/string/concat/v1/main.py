bb315d169849b2bf7131ab071f41030273e8f5c7
big-mama <ramy_h08@pop-os.localdomain>
2023-08-28 09:54:26 +0100

    feat: migrated concat and split to new sdk

--- sdk/utils/string/concat/v1/main.py
+++ sdk/utils/string/concat/v1/main.py
@@ -1,29 +1,23 @@
-from fastapi import FastAPI
-from config import Inputs, Params, Outputs
+from pydantic import Field
+from hyko_sdk import CoreModel, SDKFunction
 
-app = FastAPI()
 
-#################################################################
-
-# Insert the main code of the function here #################################################################
-
-@app.post(
-    "/load",
-    response_model=None,
+func = SDKFunction(
+    description="Concatenate two strings together",
+    requires_gpu=False,
 )
-def load():
+
+class Inputs(CoreModel):
     pass
 
-# keep the decorator, function declaration and return type the same.
-# the main function should always take Inputs as the first argument and Params as the second argument.
-# should always return Outputs.
-@app.post(
-    "/",
-    response_model=Outputs,
-)
-async def main(inputs: Inputs, params: Params):
-    return Outputs(output=params.first + params.second)
+class Params(CoreModel):
+    first: str = Field(..., description="First string")
+    second: str = Field(..., description="Second string")
+
+class Outputs(CoreModel):
+    output: str = Field(..., description="Concatenated result")
 
 
-##############################################################################################################
-
+@func.on_execute
+async def main(inputs: Inputs , params: Params)-> Outputs:
+   return Outputs(output=params.first + params.second)
