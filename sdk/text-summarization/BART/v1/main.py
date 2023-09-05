d4fd8b22550c9c211a31e287c50ee6e6c66449f0
yousra <jab@big-mama.io>
2023-09-05 14:50:02 +0100

    feat: added text-summarization/BART  model to the sdk

--- sdk/text-summarization/BART/v1/main.py
+++ sdk/text-summarization/BART/v1/main.py
@@ -6,20 +6,20 @@ from hyko_sdk import CoreModel, SDKFunction
 
 
 func = SDKFunction(
-    description="Predicts a future value based on historical data",
+    description="BART model for text summarization",
     requires_gpu=False,
 )
 
 class Inputs(CoreModel):
-    text: str = Field(..., description="Input text (16k tokens max context size)")
+    text: str = Field(..., description="Input text to summarize")
 
 class Params(CoreModel):
-    max_length: int = Field(..., description="Input text (16k tokens max context size)")
-    min_length: int = Field(..., description="Input text (16k tokens max context size)")
-    do_sample: bool = Field(..., description="Input text (16k tokens max context size)")
+    max_length: int = Field(..., description="The maximum length of the summary to generate")
+    min_length: int = Field(..., description="The minimum length of the summary to generate")
+    do_sample: bool = Field(..., description="A boolean flag indicating whether to use sampling during summary generation")
     
 class Outputs(CoreModel):
-    summary: str = Field(..., description="Summarized text")
+    summary: str = Field(..., description="The resulting summarized text")
 
 model_pipeline = None
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device('cpu')
@@ -36,7 +36,7 @@ async def load():
     model_pipeline = pipeline("summarization", model="facebook/bart-large-cnn", device_map=device)
 
 @func.on_execute
-async def main(inputs: Inputs, params: Params):
+async def main(inputs: Inputs, params: Params)-> Outputs:
 
     if model_pipeline is None:
         raise HTTPException(status_code=500, detail="Model is not loaded yet")
