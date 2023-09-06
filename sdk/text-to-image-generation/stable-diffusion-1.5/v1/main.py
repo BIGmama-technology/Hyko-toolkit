0c715d0a4173437554dad6234bcfda70ebd0ff18
yousra <jab@big-mama.io>
2023-09-06 10:33:13 +0100

    migrated stable-diffusion-1.5 to new sdk

--- sdk/text-to-image-generation/stable-diffusion-1.5/v1/main.py
+++ sdk/text-to-image-generation/stable-diffusion-1.5/v1/main.py
@@ -1,20 +1,31 @@
-import fastapi
 from fastapi.exceptions import HTTPException
-from config import Inputs, Params, Outputs, Image
 from diffusers import StableDiffusionPipeline
 import torch
 import numpy as np
+from pydantic import Field
+from hyko_sdk import CoreModel, SDKFunction, Image
 
-app = fastapi.FastAPI()
+
+func = SDKFunction(
+    description="Text to Image Generation Model",
+    requires_gpu=True,
+)
+
+class Inputs(CoreModel):
+    prompt: str = Field(..., description="User text prompt")
+
+class Params(CoreModel):
+    pass
+
+class Outputs(CoreModel):
+    generated_image: Image = Field(..., description="AI Generated image described by user text prompt")
+
 
 model_pipeline = None
 device = torch.device("cuda:1") if torch.cuda.is_available() else torch.device('cpu')
 
-@app.post(
-    "/load",
-    response_model=None
-)
-def load():
+@func.on_startup
+async def load():
     global model_pipeline
     if model_pipeline is not None:
         print("Model loaded already")
@@ -25,19 +36,15 @@ def load():
     model_pipeline = model_pipeline.to(device)
 
 
-@app.post(
-    "/",
-    response_model=Outputs
-)
-async def main(inputs: Inputs, params: Params):
+@func.on_execute
+async def main(inputs: Inputs, params: Params)-> Outputs:
     if model_pipeline is None:
         raise HTTPException(status_code=500, detail="Model is not loaded yet")
     
-    prompt = inputs.input_prompt
-    images = model_pipeline(prompt).images #type: ignore
+    prompt = inputs.prompt
+    images = model_pipeline(prompt).images      #type: ignore
     output_image_arr = np.asarray(images[0])
     print(output_image_arr.shape)
     img = Image.from_ndarray(output_image_arr)
-    await img.wait_data()
-    print(len(img.data), img.data[:100])
+    print(len(img.get_data()), img.get_data()[:100])
     return Outputs(generated_image = img)
