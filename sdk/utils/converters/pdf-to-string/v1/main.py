34caea058151b93cf51e546af0b86ece0485e114
Lotfi Lehamel <ll@big-mama.io>
2023-09-11 13:22:03 +0100

    feat: added multiprocessing text extraction

--- sdk/utils/converters/pdf-to-string/v1/main.py
+++ sdk/utils/converters/pdf-to-string/v1/main.py
@@ -1,7 +1,9 @@
-from io import BytesIO
 from pydantic import Field
 from hyko_sdk import CoreModel, SDKFunction, PDF
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PageObject
+from io import BytesIO
+import multiprocessing
+import asyncio
 
 func = SDKFunction(
     description="Convert a PDF type to String type (extracts the text from the pdf)",
@@ -18,10 +20,30 @@ class Outputs(CoreModel):
     text: str = Field(..., description="Extracted text from pdf")
 
 
+def extract_text_multi(content: bytearray):
+    
+    reader = PdfReader(BytesIO(content))
+    
+    texts = multiprocessing.Manager().list(["" for _ in range(len(reader.pages))])
+
+    processes: list[multiprocessing.Process] = []
+
+    def extract_text(page: PageObject, i: int):
+        texts[i] = page.extract_text()
+        print(f"{i} -> {len(texts[i])}")
+
+    for i, page in enumerate(reader.pages):
+        p = multiprocessing.Process(target=extract_text, args=[page, i])
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+    
+    return ''.join(texts)
+
+
 @func.on_execute
 async def main(inputs: Inputs, params: Params) -> Outputs:
-    reader = PdfReader(BytesIO(inputs.pdf_file.get_data()))
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
+    text = await asyncio.to_thread(extract_text_multi, inputs.pdf_file.get_data())
     return Outputs(text=text)
