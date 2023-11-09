39c5cb32d6974703bdb6d6c9062c01c3333b2bc7
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-11-09 11:48:56 +0100

    add_to_sdk: partial support for image segmentation

---
+++ sdk/huggingface/computer-vision/image-segmentation/v1/main.py
@@ -0,0 +1,64 @@
+"""
+TODO handle the output of segmentation, now only one mask is supported and is
+returned as a PIL image.
+"""
+from fastapi import HTTPException
+from pydantic import Field
+from hyko_sdk import CoreModel, SDKFunction, Image
+import os
+from transformers import pipeline
+
+func = SDKFunction(
+    description="HuggingFace image segmentation",
+    requires_gpu=False,
+)
+
+
+class Inputs(CoreModel):
+    input_image: Image = Field(..., description="Input image")
+
+
+class Params(CoreModel):
+    hugging_face_model: str = Field(
+        ..., description="Model"
+    )  # WARNING: DO NOT REMOVE! implementation specific
+
+
+class Outputs(CoreModel):
+    mask: Image = Field(..., description="Segmented image")
+
+
+segmenter = None
+
+
+@func.on_startup
+async def load():
+    global segmenter
+
+    if segmenter is not None:
+        print("Model already Loaded")
+        return
+
+    model = os.getenv("HYKO_HF_MODEL")
+
+    if model is None:
+        raise HTTPException(status_code=500, detail="Model env not set")
+
+    try:
+        segmenter = pipeline("image-segmentation", model=model, device_map="cpu")
+
+    except Exception as exc:
+        import logging
+
+        logging.error(exc)
+
+
+@func.on_execute
+async def main(inputs: Inputs, params: Params) -> Outputs:
+    if segmenter is None:
+        raise HTTPException(status_code=500, detail="Model is not loaded yet")
+
+    res = segmenter(inputs.input_image.to_pil())
+    mask = Image.from_pil(res[0]["mask"])
+
+    return Outputs(mask=mask)  # type: ignore
