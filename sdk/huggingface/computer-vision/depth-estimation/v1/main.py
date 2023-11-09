235e3bc06ede09197e7bfc8c55a75fabaf7a8999
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-11-09 13:31:47 +0100

    format: autoformat huggingface main modules

--- sdk/huggingface/computer-vision/depth-estimation/v1/main.py
+++ sdk/huggingface/computer-vision/depth-estimation/v1/main.py
@@ -9,11 +9,16 @@ func = SDKFunction(
     requires_gpu=False,
 )
 
+
 class Inputs(CoreModel):
     input_image: Image = Field(..., description="Input image")
 
+
 class Params(CoreModel):
-    hugging_face_model: str = Field(..., description="Model") # WARNING: DO NOT REMOVE! implementation specific
+    hugging_face_model: str = Field(
+        ..., description="Model"
+    )  # WARNING: DO NOT REMOVE! implementation specific
+
 
 class Outputs(CoreModel):
     depth_map: Image = Field(..., description="Output depth map")
@@ -21,35 +26,35 @@ class Outputs(CoreModel):
 
 estimator = None
 
+
 @func.on_startup
 async def load():
     global estimator
-    
+
     if estimator is not None:
         print("Model already Loaded")
         return
-    
+
     model = os.getenv("HYKO_HF_MODEL")
-    
+
     if model is None:
         raise HTTPException(status_code=500, detail="Model env not set")
-    
+
     try:
-        estimator = pipeline(
-        "depth-estimation", 
-        model=model,
-        device_map="cpu")
-    
+        estimator = pipeline("depth-estimation", model=model, device_map="cpu")
+
     except Exception as exc:
         import logging
+
         logging.error(exc)
 
+
 @func.on_execute
-async def main(inputs: Inputs, params: Params)-> Outputs:
+async def main(inputs: Inputs, params: Params) -> Outputs:
     if estimator is None:
         raise HTTPException(status_code=500, detail="Model is not loaded yet")
-    
+
     res = estimator(inputs.input_image.to_pil())
     depth_map = Image.from_pil(res["depth"])
-    
-    return Outputs(depth_map=depth_map) # type: ignore
+
+    return Outputs(depth_map=depth_map)  # type: ignore
--- sdk/huggingface/computer-vision/image-classification/v1/main.py
+++ sdk/huggingface/computer-vision/image-classification/v1/main.py
@@ -9,11 +9,16 @@ func = SDKFunction(
     requires_gpu=False,
 )
 
+
 class Inputs(CoreModel):
     input_image: Image = Field(..., description="Input image")
 
+
 class Params(CoreModel):
-    hugging_face_model: str = Field(..., description="Model") # WARNING: DO NOT REMOVE! implementation specific
+    hugging_face_model: str = Field(
+        ..., description="Model"
+    )  # WARNING: DO NOT REMOVE! implementation specific
+
 
 class Outputs(CoreModel):
     image_class: str = Field(..., description="Class of the image")
@@ -21,34 +26,34 @@ class Outputs(CoreModel):
 
 classifier = None
 
+
 @func.on_startup
 async def load():
     global classifier
-    
+
     if classifier is not None:
         print("Model already Loaded")
         return
-    
+
     model = os.getenv("HYKO_HF_MODEL")
-    
+
     if model is None:
         raise HTTPException(status_code=500, detail="Model env not set")
-    
+
     try:
-        classifier = pipeline(
-        "image-classification", 
-        model=model,
-        device_map="cpu")
-    
+        classifier = pipeline("image-classification", model=model, device_map="cpu")
+
     except Exception as exc:
         import logging
+
         logging.error(exc)
 
+
 @func.on_execute
-async def main(inputs: Inputs, params: Params)-> Outputs:
+async def main(inputs: Inputs, params: Params) -> Outputs:
     if classifier is None:
         raise HTTPException(status_code=500, detail="Model is not loaded yet")
-    
+
     res = classifier(inputs.input_image.to_pil())
-    
-    return Outputs(image_class=res[0]["label"]) # type: ignore
+
+    return Outputs(image_class=res[0]["label"])  # type: ignore
