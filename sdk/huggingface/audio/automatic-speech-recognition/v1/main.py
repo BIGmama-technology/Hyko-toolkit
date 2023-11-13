bc52c7285cd53bdcb448af3472dc781d9098e68a
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-11-13 13:11:42 +0100

    add_to_sdk: huggingface automatic speech recognition

---
+++ sdk/huggingface/audio/automatic-speech-recognition/v1/main.py
@@ -0,0 +1,62 @@
+from fastapi import HTTPException
+from pydantic import Field
+from hyko_sdk import CoreModel, SDKFunction, Audio
+import os
+from transformers import pipeline
+
+func = SDKFunction(
+    description="HuggingFace depth estimation",
+    requires_gpu=False,
+)
+
+
+class Inputs(CoreModel):
+    speech: Audio = Field(..., description="Input speech")
+
+
+class Params(CoreModel):
+    hugging_face_model: str = Field(
+        ..., description="Model"
+    )  # WARNING: DO NOT REMOVE! implementation specific
+
+
+class Outputs(CoreModel):
+    text: str = Field(..., description="Recognized speech text")
+
+
+recognizer = None
+
+
+@func.on_startup
+async def load():
+    global recognizer
+
+    if recognizer is not None:
+        print("Model already Loaded")
+        return
+
+    model = os.getenv("HYKO_HF_MODEL")
+
+    if model is None:
+        raise HTTPException(status_code=500, detail="Model env not set")
+
+    try:
+        recognizer = pipeline(
+            "automatic-speech-recognition", model=model, device_map="cpu"
+        )
+
+    except Exception as exc:
+        import logging
+
+        logging.error(exc)
+
+
+@func.on_execute
+async def main(inputs: Inputs, params: Params) -> Outputs:
+    if recognizer is None:
+        raise HTTPException(status_code=500, detail="Model is not loaded yet")
+
+    audio_array, sample_rate = inputs.speech.to_ndarray()
+    result = recognizer({"sampling_rate": sample_rate, "raw": audio_array})
+
+    return Outputs(text=result["text"])  # type: ignore
