4d3c951d498dba4dc49129391c341a76c9a58b80
1hachem <hb@big-mama.io>
2024-01-09 09:38:23 +0100

    feat: move to new metadata convention

--- sdk/huggingface/computer-vision/depth-estimation/v1/main.py
+++ sdk/huggingface/computer-vision/depth-estimation/v1/main.py
@@ -1,35 +1,10 @@
 import os
 
 from fastapi import HTTPException
-from pydantic import Field
+from metadata import Inputs, Outputs, Params, func
 from transformers import pipeline
 
-from hyko_sdk.function import SDKFunction
 from hyko_sdk.io import Image
-from hyko_sdk.metadata import CoreModel
-
-func = SDKFunction(
-    description="HuggingFace depth estimation",
-    requires_gpu=False,
-)
-
-
-class Inputs(CoreModel):
-    input_image: Image = Field(..., description="Input image")
-
-
-class Params(CoreModel):
-    hugging_face_model: str = Field(
-        ..., description="Model"
-    )  # WARNING: DO NOT REMOVE! implementation specific
-    device_map: str = Field(
-        ..., description="Device map (Auto, CPU or GPU)"
-    )  # WARNING: DO NOT REMOVE! implementation specific
-
-
-class Outputs(CoreModel):
-    depth_map: Image = Field(..., description="Output depth map")
-
 
 estimator = None
 
