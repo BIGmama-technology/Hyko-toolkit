d92f4972fefa5670ad6a1ba2b07f755e80a529e1
1hachem <hb@big-mama.io>
2024-01-10 18:34:19 +0100

    chore: move misc functions to using metadata

--- sdk/text-completion/falcon-40B-instruct/v1/main.py
+++ sdk/text-completion/falcon-40B-instruct/v1/main.py
@@ -1,38 +1,8 @@
 import torch
 import transformers
 from fastapi import HTTPException
-from pydantic import Field
+from metadata import Inputs, Outputs, Params, func
 from transformers import AutoTokenizer
-
-from hyko_sdk.function import SDKFunction
-from hyko_sdk.metadata import CoreModel
-
-func = SDKFunction(
-    description="instruct generation model",
-    requires_gpu=False,
-)
-
-
-class Inputs(CoreModel):
-    prompt: str = Field(..., description="User prompt to falcon-instruct")
-
-
-class Params(CoreModel):
-    system_prompt: str = Field(
-        default=None,
-        description="system-prompt or system-instruction to falcon-instruct",
-    )
-    max_length: int = Field(default=200, description="Max tokens to generate")
-    top_k: int = Field(
-        default=10, description="top_k candidates for each token generation"
-    )
-    temperature: float = Field(default=0.6, description="Temperature of falcon")
-    top_p: float = Field(default=0.6, description="Top P of falcon")
-
-
-class Outputs(CoreModel):
-    generated_text: str = Field(..., description="Generated Text from falcon-instruct")
-
 
 pipeline = None
 tokenizer = None
