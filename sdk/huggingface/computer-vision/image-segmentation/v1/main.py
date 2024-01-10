20d132e4e869297c2434b56dc01babb988711620
1hachem <hb@big-mama.io>
2024-01-10 10:58:52 +0100

    feat: huggingface computer vision move to new metadata convention

--- sdk/huggingface/computer-vision/image-segmentation/v1/main.py
+++ sdk/huggingface/computer-vision/image-segmentation/v1/main.py
@@ -5,35 +5,10 @@ returned as a PIL image.
 import os
 
 from fastapi import HTTPException
-from pydantic import Field
+from metadata import Inputs, Outputs, Params, func
 from transformers import pipeline
 
-from hyko_sdk.function import SDKFunction
 from hyko_sdk.io import Image
-from hyko_sdk.metadata import CoreModel
-
-func = SDKFunction(
-    description="HuggingFace image segmentation",
-    requires_gpu=False,
-)
-
-
-class Inputs(CoreModel):
-    input_image: Image = Field(..., description="Input image")
-
-
-class Params(CoreModel):
-    hugging_face_model: str = Field(
-        ..., description="Model"
-    )  # WARNING: DO NOT REMOVE! implementation specific
-    device_map: str = Field(
-        ..., description="Device map (Auto, CPU or GPU)"
-    )  # WARNING: DO NOT REMOVE! implementation specific
-
-
-class Outputs(CoreModel):
-    mask: Image = Field(..., description="Segmented image")
-
 
 segmenter = None
 
@@ -68,4 +43,4 @@ async def main(inputs: Inputs, params: Params) -> Outputs:
     res = segmenter(inputs.input_image.to_pil())
     mask = Image.from_pil(res[0]["mask"])
 
-    return Outputs(mask=mask)  # type: ignore
+    return Outputs(mask=mask)
