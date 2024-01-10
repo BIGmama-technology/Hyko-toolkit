4db5d71e10b6860d014441f52c02eccc1294b1ce
1hachem <hb@big-mama.io>
2024-01-10 12:02:33 +0100

    feat: huggingface multi-modal move to new metadata convention

--- sdk/huggingface/multimodal/visual-question-answering/v1/main.py
+++ sdk/huggingface/multimodal/visual-question-answering/v1/main.py
@@ -1,38 +1,8 @@
 import os
 
 from fastapi import HTTPException
-from pydantic import Field
+from metadata import Inputs, Outputs, Params, func
 from transformers import pipeline
-
-from hyko_sdk.function import SDKFunction
-from hyko_sdk.io import Image
-from hyko_sdk.metadata import CoreModel
-
-func = SDKFunction(
-    description="Hugging Face Image-To-Text Task",
-    requires_gpu=False,
-)
-
-
-class Inputs(CoreModel):
-    image: Image = Field(..., description="Input image")
-    question: str = Field(..., description="Input question")
-
-
-class Params(CoreModel):
-    hugging_face_model: str = Field(
-        ..., description="Model"
-    )  # WARNING: DO NOT REMOVE! implementation specific
-    device_map: str = Field(
-        ..., description="Device map (Auto, CPU or GPU)"
-    )  # WARNING: DO NOT REMOVE! implementation specific
-    top_k: int = Field(default=1, description="Top K")
-
-
-class Outputs(CoreModel):
-    answer: str = Field(..., description="Generated answer")
-    score: float = Field(..., description="Confidance score")
-
 
 vqa_pipeline = None
 
