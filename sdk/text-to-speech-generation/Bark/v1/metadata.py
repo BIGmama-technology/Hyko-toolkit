1f1972a57537e625a61284be473d135366626160
1hachem <hb@big-mama.io>
2024-01-10 14:43:14 +0100

    feat: move text to speech to new metadata convention

---
+++ sdk/text-to-speech-generation/Bark/v1/metadata.py
@@ -0,0 +1,35 @@
+from pydantic import Field
+
+from hyko_sdk.function import SDKFunction
+from hyko_sdk.io import Audio
+from hyko_sdk.metadata import CoreModel
+
+func = SDKFunction(
+    description="Generate audio from a given prompt",
+    requires_gpu=False,
+)
+
+
+@func.set_input
+class Inputs(CoreModel):
+    text: str = Field(..., description="Prompt for audio generation")
+
+
+@func.set_param
+class Params(CoreModel):
+    history: str = Field(
+        default=None, description="History prompts for audio generation"
+    )
+    text_tempreture: float = Field(
+        default=0.5,
+        description="Generation temperature (1.0 more diverse, 0.0 more conservative)",
+    )
+    waveform_temp: float = Field(
+        default=0.5,
+        description="generation temperature (1.0 more diverse, 0.0 more conservative",
+    )
+
+
+@func.set_output
+class Outputs(CoreModel):
+    audio: Audio = Field(..., description="Generated audio")
