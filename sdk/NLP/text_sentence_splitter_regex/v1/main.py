0a3e0f7f89dc6c4cf1a7b1176ab0e13a7bb26cf8
1hachem <hb@big-mama.io>
2024-01-10 18:34:19 +0100

    chore: move misc functions to using metadata

--- sdk/NLP/text_sentence_splitter_regex/v1/main.py
+++ sdk/NLP/text_sentence_splitter_regex/v1/main.py
@@ -1,32 +1,7 @@
 import logging
 import re
 
-from pydantic import Field
-
-from hyko_sdk.function import SDKFunction
-from hyko_sdk.metadata import CoreModel
-
-func = SDKFunction(
-    description="Takes a paragraph and tokenizes (splits) it into a list of sentences",
-    requires_gpu=False,
-)
-
-
-class Inputs(CoreModel):
-    text: str = Field(..., description="Text to be split")
-
-
-class Params(CoreModel):
-    pass
-
-
-class Outputs(CoreModel):
-    sentences: list[str] = Field(..., description="Splitted sentences")
-
-
-@func.on_startup
-async def init():
-    return
+from metadata import Inputs, Outputs, Params, func
 
 
 @func.on_execute
