61127f010ade16c136683b494bacf7e9b24c6c72
1hachem <hb@big-mama.io>
2024-01-10 18:34:19 +0100

    chore: move misc functions to using metadata

--- sdk/regression/linear-regression/v1/main.py
+++ sdk/regression/linear-regression/v1/main.py
@@ -1,33 +1,7 @@
-from typing import List
-
 import numpy as np
 from fastapi.exceptions import HTTPException
-from pydantic import Field
+from metadata import Inputs, Outputs, Params, func
 from sklearn.linear_model import LinearRegression
-
-from hyko_sdk.function import SDKFunction
-from hyko_sdk.metadata import CoreModel
-
-func = SDKFunction(
-    description="Predicts a future value based on historical data",
-    requires_gpu=False,
-)
-
-
-class Inputs(CoreModel):
-    predict_x: float = Field(
-        ..., description="X axis value for which to predict Y axis value"
-    )
-
-
-class Params(CoreModel):
-    historical_x: List[float] = Field(..., description="Historical data of X axis")
-    historical_y: List[float] = Field(..., description="Historical data of Y axis")
-
-
-class Outputs(CoreModel):
-    predict_y: float = Field(..., description="Predicted Y axis value")
-
 
 linear_regression = LinearRegression()
 
