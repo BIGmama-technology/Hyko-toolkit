533e21d26b912afe349de985b04b418e390bc9e4
1hachem <hb@big-mama.io>
2024-01-10 13:33:38 +0100

    feat: move utils math to new metadata convention

--- sdk/utils/math/Range/v1/main.py
+++ sdk/utils/math/Range/v1/main.py
@@ -1,26 +1,5 @@
 from fastapi import HTTPException
-from pydantic import Field
-
-from hyko_sdk.function import SDKFunction
-from hyko_sdk.metadata import CoreModel
-
-func = SDKFunction(
-    description="Generate a range of integers",
-    requires_gpu=False,
-)
-
-
-class Inputs(CoreModel):
-    pass
-
-
-class Params(CoreModel):
-    start: int = Field(..., description="Start integer in the range")
-    end: int = Field(..., description="End integer (not included in the range)")
-
-
-class Outputs(CoreModel):
-    result: list[int] = Field(..., description="Output range")
+from metadata import Inputs, Outputs, Params, func
 
 
 @func.on_execute
