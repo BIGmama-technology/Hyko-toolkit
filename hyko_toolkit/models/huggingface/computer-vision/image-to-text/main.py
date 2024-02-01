4029e55da32edcaa190e3521cc80ebe727acde52
1hachem <hb@big-mama.io>
2024-02-01 13:57:27 +0100

    feat: move to startup params

--- hyko_toolkit/models/huggingface/computer-vision/image-to-text/main.py
+++ hyko_toolkit/models/huggingface/computer-vision/image-to-text/main.py
@@ -1,22 +1,13 @@
-import os
-
-from fastapi import HTTPException
-from metadata import Inputs, Outputs, Params, func
+from metadata import Inputs, Outputs, Params, StartupParams, func
 from transformers import pipeline
-
-captioner = None
 
 
 @func.on_startup
-async def load():
+async def load(startup_params: StartupParams):
     global captioner
 
-    model = os.getenv("HYKO_HF_MODEL")
-
-    if model is None:
-        raise HTTPException(status_code=500, detail="Model env not set")
-
-    device_map = os.getenv("HYKO_DEVICE_MAP", "auto")
+    model = startup_params.hugging_face_model
+    device_map = startup_params.device_map
 
     captioner = pipeline(
         "image-to-text",
@@ -27,9 +18,6 @@ async def load():
 
 @func.on_execute
 async def main(inputs: Inputs, params: Params) -> Outputs:
-    if captioner is None:
-        raise HTTPException(status_code=500, detail="Model is not loaded yet")
-
     res = captioner(inputs.input_image.to_pil())
 
     return Outputs(caption=res[0]["generated_text"])  # type: ignore
