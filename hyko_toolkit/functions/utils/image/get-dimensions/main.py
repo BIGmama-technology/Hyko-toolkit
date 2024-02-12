f228a2963f7568bcec5c858b536fd1ab58dc5cec
Ouassim Abdelmalek Ghribi <oag@big-mama.io>
2024-02-12 17:58:59 +0100

    chore: removed type: ignore in functions that take images as inputs

--- hyko_toolkit/functions/utils/image/get-dimensions/main.py
+++ hyko_toolkit/functions/utils/image/get-dimensions/main.py
@@ -6,9 +6,9 @@ from PIL import Image as PIL_Image
 
 @func.on_execute
 async def main(inputs: Inputs, params: Params) -> Outputs:
-    _, ext = os.path.splitext(inputs.image.get_name())  # type: ignore
+    _, ext = os.path.splitext(inputs.image.get_name())
     with open(f"./image{ext}", "wb") as f:
-        f.write(inputs.image.get_data())  # type: ignore
+        f.write(inputs.image.get_data())
 
     image = PIL_Image.open(f"./image{ext}")
     width, height = image.size
