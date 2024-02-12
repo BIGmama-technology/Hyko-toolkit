4f4d4255c56f4f2ae95b03d6cf0bfe438d8e0a3c
ayoubmrx <ak@big-mama.io>
2024-02-12 16:23:42 +0100

    feat: added web scraping-function

---
+++ hyko_toolkit/functions/utils/web_scraping/beautifulsoup_transformer/metadata.py
@@ -0,0 +1,30 @@
+from pydantic import Field
+
+from hyko_sdk.function import SDKFunction
+from hyko_sdk.metadata import CoreModel
+
+func = SDKFunction(
+    description="Scrape HTML content from URLs and convert it to plain text"
+)
+
+
+@func.set_input
+class Inputs(CoreModel):
+    urls: list[str] = Field(
+        ...,
+        description="A list of URLs to scrape. Protocol must be either 'http' or 'https'.",
+    )
+
+
+@func.set_param
+class Params(CoreModel):
+    tags_to_extract: str = Field(
+        ..., description="A list of HTML tags to extract content from"
+    )
+
+
+@func.set_output
+class Outputs(CoreModel):
+    result: list[str] = Field(
+        ..., description="List of transformed documents as plain text."
+    )
