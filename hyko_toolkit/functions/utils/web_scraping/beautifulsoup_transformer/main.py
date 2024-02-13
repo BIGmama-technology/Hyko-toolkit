bde69a9b56929f6a130e309df9364d4594a9a336
ayoubmrx <ak@big-mama.io>
2024-02-13 09:38:21 +0100

    feat: Separate document results with URLs

--- hyko_toolkit/functions/utils/web_scraping/beautifulsoup_transformer/main.py
+++ hyko_toolkit/functions/utils/web_scraping/beautifulsoup_transformer/main.py
@@ -20,7 +20,12 @@ async def main(inputs: Inputs, params: Params) -> Outputs:
     loader = AsyncChromiumLoader(urls=inputs.urls)
     docs = loader.load()
     bs_transformer = BeautifulSoupTransformer()
-    tags = params.tags_to_extract.split("+")
+    tags = "".join(params.tags_to_extract.split()).split(",")
     docs_transformed = bs_transformer.transform_documents(docs, tags)
-    page_contents = [text.page_content for text in docs_transformed]
+    # Assuming docs_transformed is a list of transformed documents
+    page_contents = []
+    # Iterate over each transformed document and concatenate it with the URL header
+    for i, text in enumerate(docs_transformed):
+        url_header = f"\n=== URL {i+1} {inputs.urls[i]} === \n"
+        page_contents.append(f"{url_header}\n{text.page_content}")
     return Outputs(result=page_contents)
