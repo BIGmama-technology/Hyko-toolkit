6ec194aefa6c54800cf8f5a94b3f1a8e16668e86
ayoubmrx <ak@big-mama.io>
2024-02-15 11:29:18 +0100

    feat: Add youtube_transcripts Tool

---
+++ hyko_toolkit/functions/NLP/langchain/youtube_transcripts/metadata.py
@@ -0,0 +1,31 @@
+from pydantic import Field
+
+from hyko_sdk.function import SDKFunction
+from hyko_sdk.metadata import CoreModel
+
+func = SDKFunction(description="")
+
+
+@func.set_input
+class Inputs(CoreModel):
+    url: str = Field(
+        ...,
+        description="Youtube URL. ",
+    )
+
+
+@func.set_param
+class Params(CoreModel):
+    language: str = Field(
+        ...,
+        description="Originale Language Id .example : en , ar.",
+    )
+    translation: str = Field(
+        ...,
+        description="Translation Language Id .example : en , ar",
+    )
+
+
+@func.set_output
+class Outputs(CoreModel):
+    result: str = Field(..., description="Result")
