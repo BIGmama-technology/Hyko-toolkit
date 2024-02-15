b84abb9096b80cb299b990148326ce23ede9a178
ayoubmrx <ak@big-mama.io>
2024-02-15 11:38:39 +0100

    refactor: add description

--- hyko_toolkit/functions/NLP/langchain/youtube_transcripts/main.py
+++ hyko_toolkit/functions/NLP/langchain/youtube_transcripts/main.py
@@ -4,6 +4,18 @@ from metadata import Inputs, Outputs, Params, func
 
 @func.on_execute
 async def main(inputs: Inputs, params: Params) -> Outputs:
+    """
+    Loads content from a YouTube video specified by the input URL and extracts relevant documents.
+    The content can be loaded with optional language and translation parameters.
+    The function returns the extracted content as output.
+
+    Args:
+        inputs (Inputs): Input data containing the YouTube video URL.
+        params (Params): Parameters for the function, including language and translation settings.
+
+    Returns:
+        Outputs: Extracted content from the YouTube video.
+    """
     loader = YoutubeLoader.from_youtube_url(
         inputs.url,
         add_video_info=False,
