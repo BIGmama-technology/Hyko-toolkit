41875a3d64e61734d0579d97297fe018633f3f1a
ayoubmrx <ak@big-mama.io>
2024-02-16 14:17:17 +0100

    feat: Add wikipedia_search Tool

---
+++ hyko_toolkit/functions/utils/web_search/wikipedia_search/metadata.py
@@ -0,0 +1,31 @@
+from pydantic import Field
+
+from hyko_sdk.function import SDKFunction
+from hyko_sdk.metadata import CoreModel
+
+func = SDKFunction(description="Search the web using DuckDuckGo .")
+
+
+@func.set_input
+class Inputs(CoreModel):
+    query: str = Field(
+        ...,
+        description="The search query .",
+    )
+
+
+@func.set_param
+class Params(CoreModel):
+    num_results: int = Field(
+        ...,
+        description="Number of search results to return .",
+    )
+    language: str = Field(
+        ...,
+        description="The search Language (Ex : en or ar) .",
+    )
+
+
+@func.set_output
+class Outputs(CoreModel):
+    result: str = Field(..., description="The concatenated titles and summaries .")
