2525475c7dad64666fb127c618c13e8eb8410db8
1hachem <hb@big-mama.io>
2024-02-17 17:06:05 +0100

    refactor: remove prompt former and word counter

--- hyko_toolkit/functions/NLP/word_counter/main.py
+++
@@ -1,6 +0,0 @@
-from metadata import Inputs, Outputs, Params, func
-
-
-@func.on_execute
-async def main(inputs: Inputs, params: Params) -> Outputs:
-    return Outputs(count=len(inputs.text.split(" ")))
