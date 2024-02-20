583c8fc7aa39a9dd987a7e7d324dbbe3c62918ff
ayoubmrx <ak@big-mama.io>
2024-02-20 15:22:11 +0100

    refactor: add (max_new_tokens,temperature,top_p,top_k) parameters to image-to-text pipeline

--- hyko_toolkit/models/huggingface/computer-vision/image-to-text/metadata.py
+++ hyko_toolkit/models/huggingface/computer-vision/image-to-text/metadata.py
@@ -22,7 +22,14 @@ class Inputs(CoreModel):
 
 @func.set_param
 class Params(CoreModel):
-    pass
+    max_new_tokens: int = Field(
+        default=20, description="Cap newly generated content length (default=20)."
+    )
+    temperature: float = Field(default=0.6, description="Randomness (default=0.8).")
+    top_p: float = Field(
+        default=0.7, description="Focus high-probability words (default=0.7)"
+    )
+    top_k: int = Field(default=1, description="Keep best k options (default=1).")
 
 
 @func.set_output
