9b01eb726bc4421913367c1736381f013795cec1
ayoubmrx <ak@big-mama.io>
2024-02-21 10:08:55 +0100

    refactor: add top_k and frame_sampling_rate  parametre to video-classification pipeline

--- hyko_toolkit/models/huggingface/computer-vision/video-classification/main.py
+++ hyko_toolkit/models/huggingface/computer-vision/video-classification/main.py
@@ -25,7 +25,11 @@ async def main(inputs: Inputs, params: Params) -> Outputs:
     with open(f"/app/video.{ext}", "wb") as f:
         f.write(inputs.input_video.get_data())
 
-    res = segmenter(f"/app/video.{ext}")
+    res = segmenter(
+        f"/app/video.{ext}",
+        top_k=params.top_k,
+        frame_sampling_rate=params.frame_sampling_rate,
+    )
     labels = [prediction["label"] for prediction in res]
     scores = [prediction["score"] for prediction in res]
     return Outputs(labels=labels, scores=scores)
