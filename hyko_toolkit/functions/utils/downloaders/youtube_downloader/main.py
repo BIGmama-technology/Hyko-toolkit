6b767f93fa410a9fcddbfab5648aab63aee92bf7
ayoubmrx <ak@big-mama.io>
2024-02-26 08:17:51 +0100

    refactor: pass the bytes directly to val in youtube_downloader

--- hyko_toolkit/functions/utils/downloaders/youtube_downloader/main.py
+++ hyko_toolkit/functions/utils/downloaders/youtube_downloader/main.py
@@ -1,4 +1,4 @@
-import os
+import io
 
 from metadata import (
     Inputs,
@@ -12,17 +12,16 @@ from hyko_sdk.io import Video
 from hyko_sdk.types import Ext
 
 
-def download_video(url, output_path, resolution):
+def download_video(url, resolution):
     """
     Download a video from YouTube.
 
     Args:
     - url (str): The URL of the YouTube video to download.
-    - output_path (str): The directory where the downloaded video will be saved.
     - resolution (str): The desired resolution of the video. Options: 'highest', 'lowest', or specific resolution like '720p', '360p', etc. Default is 'highest'.
 
     Returns:
-    - str: The file path of the downloaded video if successful, otherwise None.
+    - bytes: The video content as bytes if successful, otherwise None.
     """
     try:
         yt = YouTube(url)
@@ -32,20 +31,20 @@ def download_video(url, output_path, resolution):
             stream = yt.streams.get_lowest_resolution()
         else:
             stream = yt.streams.filter(res=resolution).first()
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-        file_path = stream.download(output_path)
-        return file_path
-    except Exception:
+
+        # Download video to memory buffer
+        video_buffer = io.BytesIO()
+        stream.stream_to_buffer(video_buffer)
+        video_content = video_buffer.getvalue()
+        video_buffer.close()
+
+        return video_content
+    except Exception as e:
+        print("An error occurred:", str(e))
         return None
 
 
 @func.on_execute
 async def main(inputs: Inputs, params: Params) -> Outputs:
-    downloaded_file_path = download_video(
-        inputs.video_url, output_path="results", resolution=params.resolution.value
-    )
-
-    with open(downloaded_file_path, "rb") as f:
-        data = f.read()
+    data = download_video(url=inputs.video_url, resolution=params.resolution.value)
     return Outputs(output_video=Video(val=data, obj_ext=Ext.MP4))
