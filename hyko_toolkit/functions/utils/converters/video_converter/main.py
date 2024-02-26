df7278f55c582670ab4ab4c29acc3a4f356c9063
ayoubmrx <ak@big-mama.io>
2024-02-26 11:56:49 +0100

    refactor: pass and return data in bytes format in video_converter

--- hyko_toolkit/functions/utils/converters/video_converter/main.py
+++ hyko_toolkit/functions/utils/converters/video_converter/main.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 import cv2
 from metadata import Inputs, Outputs, Params, func
@@ -7,33 +8,50 @@ from hyko_sdk.io import Video
 from hyko_sdk.types import Ext
 
 
-def convert_video(input_file, output_file, codec):
+def convert_video(buffer, codec):
     """
     Convert a video from one format to another.
 
     Parameters:
-        input_file (str): Path to the input video file.
-        output_file (str): Path to the output video file.
-        codec (str).
+        buffer (bytes): The video data in bytes format.
+        codec (str): The four-character code for the codec.
+    Returns:
+        bytes: The converted video data.
     """
-    cap = cv2.VideoCapture(input_file)
+    # create a temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(buffer)
+        temp_file_path = temp_file.name
+
+    cap = cv2.VideoCapture(temp_file_path)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS)
     fourcc = cv2.VideoWriter_fourcc(*codec)
-    out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+
+    # OpenCV requires dimensions in (width, height) format
+    out = cv2.VideoWriter(temp_file_path, fourcc, fps, (width, height))
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         out.write(frame)
+
     cap.release()
     out.release()
+
+    # Read the converted video data from the temporary file
+    with open(temp_file_path, "rb") as file:
+        video_buffer = file.read()
+
+    # Remove the temporary file
+    os.unlink(temp_file_path)
+    return video_buffer
 
 
 @func.on_execute
 async def main(inputs: Inputs, params: Params) -> Outputs:
-    _, ext = os.path.splitext(inputs.input_video.get_name())
     type_to_object = {
         "webm": Ext.WEBM,
         "mp4": Ext.MP4,
@@ -43,19 +61,10 @@ async def main(inputs: Inputs, params: Params) -> Outputs:
         "wmv": Ext.WMV,
         "gif": Ext.GIF,
     }
-
-    with open(f"/app/video{ext}", "wb") as f:
-        f.write(inputs.input_video.get_data())
-
-    output_file = f"/app/result_video.{params.target_type.value}"
-
     if params.target_type == "webm":
-        convert_video(f"/app/video{ext}", output_file, codec="VP90")
+        video_buffer = convert_video(inputs.input_video.get_data(), codec="VP90")
     else:
-        convert_video(f"/app/video{ext}", output_file, codec="mp4v")
-
-    with open(output_file, "rb") as f:
-        data = f.read()
+        video_buffer = convert_video(inputs.input_video.get_data(), codec="mp4v")
     return Outputs(
-        output_video=Video(val=data, obj_ext=type_to_object[params.target_type])
+        output_video=Video(val=video_buffer, obj_ext=type_to_object[params.target_type])
     )
