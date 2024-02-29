a43771a2d71e38195af892080d8688d21fdbe326
ayoubmrx <ak@big-mama.io>
2024-02-29 10:29:57 +0100

    refactor: add generate_plots_with_one_inputs

---
+++ hyko_toolkit/functions/utils/plotting_functions/generate_plots_with_one_input/metadata.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+from pydantic import Field
+
+from hyko_sdk.function import SDKFunction
+from hyko_sdk.io import Image
+from hyko_sdk.metadata import CoreModel
+
+
+class SupportedPlots(str, Enum):
+    Pie_Chart = "Pie_Chart"
+    Histogram = "Histogram"
+
+
+func = SDKFunction(
+    description="Generate various types of plots with Y input.",
+)
+
+
+@func.set_input
+class Inputs(CoreModel):
+    y: list[float] = Field(default=None, description="Y")
+
+
+@func.set_param
+class Params(CoreModel):
+    plot_type: SupportedPlots = Field(..., description="Select Plo Type.")
+
+
+@func.set_output
+class Outputs(CoreModel):
+    image: Image = Field(..., description="Output image.")
