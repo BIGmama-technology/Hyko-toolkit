133e1c43743825338d07a08af20c8ff306d9a333
ayoubmrx <ak@big-mama.io>
2024-03-03 12:15:19 +0100

    refactor: remove type_to_object mapping and replace it with Enum in image_converter tool

--- hyko_toolkit/functions/utils/converters/image_converter/main.py
+++ hyko_toolkit/functions/utils/converters/image_converter/main.py
@@ -3,7 +3,6 @@ import numpy as np
 from metadata import Inputs, Outputs, Params, func
 
 from hyko_sdk.io import Image
-from hyko_sdk.types import Ext
 
 
 @func.on_execute
@@ -18,28 +17,9 @@ async def main(inputs: Inputs, params: Params) -> Outputs:
     Returns:
     - Outputs: An object containing the converted image data.
     """
-    type_to_object = {
-        "png": Ext.PNG,
-        "jpeg": Ext.JPEG,
-        "jpg": Ext.JPG,
-        "tiff": Ext.TIFF,
-        "tif": Ext.TIF,
-        "bmp": Ext.BMP,
-        "jp2": Ext.JP2,
-        "dib": Ext.DIB,
-        "ppm": Ext.PPM,
-        "pnm": Ext.PNM,
-        "ras": Ext.RAS,
-        "hdr": Ext.HDR,
-        "webp": Ext.WEBP,
-    }
     image_np = np.frombuffer(inputs.input_image.get_data(), dtype=np.uint8)
     image = cv2.imdecode(image_np, flags=cv2.IMREAD_COLOR)
-    success, encoded_image = cv2.imencode(f".{params.target_type.value}", image)
+    success, encoded_image = cv2.imencode(f".{params.target_type.name}", image)
     target_buffer = encoded_image.tobytes()
     if success:
-        return Outputs(
-            image=Image(
-                val=target_buffer, obj_ext=type_to_object[params.target_type.value]
-            )
-        )
+        return Outputs(image=Image(val=target_buffer, obj_ext=params.target_type.value))
