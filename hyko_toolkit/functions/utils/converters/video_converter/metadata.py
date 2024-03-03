ca5ffaa9f4a01e5e3811722e518c718ccd6867be
ayoubmrx <ak@big-mama.io>
2024-03-03 12:16:57 +0100

    refactor: remove type_to_object mapping and replace it with Enum in video_converter tool

--- hyko_toolkit/functions/utils/converters/video_converter/metadata.py
+++ hyko_toolkit/functions/utils/converters/video_converter/metadata.py
@@ -5,20 +5,21 @@ from pydantic import Field
 from hyko_sdk.function import SDKFunction
 from hyko_sdk.io import Video
 from hyko_sdk.metadata import CoreModel
+from hyko_sdk.types import Ext
 
 func = SDKFunction(
     description="Convert a video from one format to another.",
 )
 
 
-class SupportedTypes(str, Enum):
-    mkv = "mkv"
-    mov = "mov"
-    avi = "avi"
-    mp4 = "mp4"
-    wmv = "wmv"
-    webm = "webm"
-    gif = "gif"
+class SupportedTypes(Enum):
+    webm = Ext.WEBM
+    mp4 = Ext.MP4
+    avi = Ext.AVI
+    mkv = Ext.MKV
+    mov = Ext.MOV
+    wmv = Ext.WMV
+    gif = Ext.GIF
 
 
 @func.set_input
