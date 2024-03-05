afd2be039edf87f14993e53e974ac6f3a1f5f510
ayoubmrx <ak@big-mama.io>
2024-03-05 11:41:37 +0100

    feat: add easyocr tool

---
+++ hyko_toolkit/models/optical_character_recognition/easyocr/metadata.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+from pydantic import Field
+
+from hyko_sdk.function import SDKFunction
+from hyko_sdk.io import Image
+from hyko_sdk.metadata import CoreModel
+
+
+class SupportedLanguages(str, Enum):
+    arabic = "ar"
+    english = "en"
+    french = "fr"
+    spanish = "es"
+
+
+func = SDKFunction(
+    description="Extracts text from an image using EasyOCR.",
+)
+
+
+@func.set_input
+class Inputs(CoreModel):
+    image: Image = Field(..., description="Input image.")
+
+
+@func.set_param
+class Params(CoreModel):
+    language: SupportedLanguages = Field(..., description="Select your language.")
+
+
+@func.set_output
+class Outputs(CoreModel):
+    generated_text: str = Field(..., description="Extracted text.")
