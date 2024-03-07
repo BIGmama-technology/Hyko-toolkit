0ca00a69165de4244d8693bc3a36d8d71d32ebef
ayoubmrx <ak@big-mama.io>
2024-03-07 11:09:36 +0100

    feat: add hybrid_search tool

---
+++ hyko_toolkit/models/similarity_search/hybrid_search/metadata.py
@@ -0,0 +1,42 @@
+from pydantic import Field
+
+from hyko_sdk.function import SDKFunction
+from hyko_sdk.metadata import CoreModel
+
+func = SDKFunction(
+    description="Perform Hybrid Search on a list of documents based on a given query."
+)
+
+
+@func.set_startup_params
+class StartupParams(CoreModel):
+    pass
+
+
+@func.set_input
+class Inputs(CoreModel):
+    docs: list[str] = Field(
+        ...,
+        description="Input Documents.",
+    )
+    query: str = Field(
+        ...,
+        description="Query or the Question to compare against the input text.",
+    )
+
+
+@func.set_param
+class Params(CoreModel):
+    bm25_k: int = Field(
+        ...,
+        description="Number of top results to consider in Best Matching Algorithm (BM25).",
+    )
+    faiss_k: int = Field(
+        ...,
+        description="Number of top results to consider in Similarity Search Algorithm.",
+    )
+
+
+@func.set_output
+class Outputs(CoreModel):
+    result: list[str] = Field(..., description="Top K results.")
