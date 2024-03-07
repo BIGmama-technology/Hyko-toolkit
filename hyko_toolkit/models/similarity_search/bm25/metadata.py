bd9b02facf90eeaf75d7e4b73aedda37ea7828fc
ayoubmrx <ak@big-mama.io>
2024-03-07 10:09:55 +0100

    feat: add bm25 retrieval tool

---
+++ hyko_toolkit/models/similarity_search/bm25/metadata.py
@@ -0,0 +1,33 @@
+from pydantic import Field
+
+from hyko_sdk.function import SDKFunction
+from hyko_sdk.metadata import CoreModel
+
+func = SDKFunction(
+    description="Perform BM25 retrieval on a list of documents based on a given query."
+)
+
+
+@func.set_input
+class Inputs(CoreModel):
+    docs: list[str] = Field(
+        ...,
+        description="Input Documents.",
+    )
+    query: str = Field(
+        ...,
+        description="Query or the Question to compare against the input text.",
+    )
+
+
+@func.set_param
+class Params(CoreModel):
+    top_k: int = Field(
+        ...,
+        description="Number of top results to consider.",
+    )
+
+
+@func.set_output
+class Outputs(CoreModel):
+    result: list[str] = Field(..., description="Top K results.")
