8ce7538b8c0e1483fe2451a1333ad8650288458e
1hachem <hb@big-mama.io>
2024-03-09 23:28:41 +0100

    fix: move to new definition

--- hyko_toolkit/functions/web/web_scraping/beautifulsoup_transformer/metadata.py
+++ hyko_toolkit/functions/web/web_scraping/beautifulsoup_transformer/metadata.py
@@ -1,10 +1,12 @@
 from pydantic import Field
 
-from hyko_sdk.definitions import SDKFunction
+from hyko_sdk.definitions import ToolkitFunction
 from hyko_sdk.models import CoreModel
 
-func = SDKFunction(
-    description="Scrape HTML content from URLs and convert it to plain text"
+func = ToolkitFunction(
+    name="beautifulsoup_transformer",
+    task="web_scraping",
+    description="Scrape HTML content from URLs and convert it to plain text",
 )
 
 
