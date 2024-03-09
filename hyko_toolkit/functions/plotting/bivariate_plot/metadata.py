c54b44ca7e97d366749983d7d89a1eb469dde2bf
1hachem <hb@big-mama.io>
2024-03-09 23:25:42 +0100

    fix: move to new definition, fix import

--- hyko_toolkit/functions/plotting/bivariate_plot/metadata.py
+++ hyko_toolkit/functions/plotting/bivariate_plot/metadata.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 from pydantic import Field
 
-from hyko_sdk.definitions import SDKFunction
+from hyko_sdk.definitions import ToolkitFunction
 from hyko_sdk.io import Image
 from hyko_sdk.models import CoreModel
 
@@ -18,7 +18,9 @@ class SupportedPlots(str, Enum):
     Scatter_Plot = "Scatter_Plot"
 
 
-func = SDKFunction(
+func = ToolkitFunction(
+    name="bivariate_plot",
+    task="plotting",
     description="Generate various types of plots with (X , Y) inputs.",
 )
 
