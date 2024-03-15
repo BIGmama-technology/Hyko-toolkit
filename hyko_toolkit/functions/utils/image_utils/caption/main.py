7df244c2a572d1414ee683d5eba1b487afc43438
1hachem <hb@big-mama.io>
2024-03-15 15:42:09 +0100

    feat: use pil instead of cv2

--- hyko_toolkit/functions/utils/image_utils/caption/main.py
+++ hyko_toolkit/functions/utils/image_utils/caption/main.py
@@ -1,82 +1,55 @@
-import os
-
-import cv2
 import numpy as np
 from fastapi.exceptions import HTTPException
-from hyko_sdk.io import Image
+from hyko_sdk.io import Image as HykoImage
 from metadata import CaptionColor, CaptionPosition, Inputs, Outputs, Params, func
-from PIL import Image as PIL_Image
+from PIL import Image, ImageDraw, ImageFont
 
 
 def add_caption(
-    img: np.ndarray,
+    img: Image.Image,
     caption: str,
     size: int,
     position: CaptionPosition,
     color: CaptionColor,
-) -> np.ndarray:
-    font = cv2.FONT_HERSHEY_SIMPLEX
+) -> Image.Image:
+    draw = ImageDraw.Draw(img)
 
-    if size < 0:
-        raise HTTPException(
-            status_code=500, detail="Caption size must be a non-negative integer"
-        )
-    else:
-        font_scale = size / 100
-        font_thickness = 2
+    font_size = int(size / 100 * 10)  # Adjust font size scale
 
-    if color == CaptionColor.BLACK:
-        font_color = (0, 0, 0)
-    elif color == CaptionColor.WHITE:
-        font_color = (255, 255, 255)
-    else:
-        raise HTTPException(
-            status_code=500, detail="Caption color must be 'black' or 'white'"
-        )
+    try:
+        font = ImageFont.truetype("arial.ttf", font_size)
+    except OSError:
+        font = ImageFont.load_default()
 
-    line_type = cv2.LINE_AA
-
-    text_size = cv2.getTextSize(caption, font, font_scale, font_thickness)[0]
-    text_width, text_height = text_size
+    _, text_height = draw.textsize(caption, font=font)  # type: ignore
 
     if position == CaptionPosition.TOP:
-        caption_y = text_height + 10
+        caption_xy = (10, 10)
     elif position == CaptionPosition.BOTTOM:
-        caption_y = img.shape[0] - 10
+        caption_xy = (10, img.height - text_height - 10)  # type: ignore
     else:
         raise HTTPException(
             status_code=500, detail="Caption position must be 'top' or 'bottom'"
         )
 
-    img = cv2.putText(
-        img,
-        caption,
-        (10, caption_y),
-        font,
-        font_scale,
-        font_color,
-        font_thickness,
-        line_type,
-    )
+    draw.text(caption_xy, caption, fill=color.value)  # type: ignore
 
     return img
 
 
 @func.on_execute
 async def main(inputs: Inputs, params: Params) -> Outputs:
-    file, ext = os.path.splitext(inputs.input_image.get_name())
-    with open(f"./image{ext}", "wb") as f:
-        f.write(inputs.input_image.get_data())
-
-    img_np = np.array(PIL_Image.open(f"./image{ext}"))
+    img_pil = Image.fromarray(inputs.input_image.to_ndarray())
 
     caption = params.caption
     size = params.caption_size
     position = params.position
     color = params.caption_color
 
-    captioned_img_np = add_caption(img_np, caption, size, position, color)
+    captioned_img_pil = add_caption(img_pil, caption, size, position, color)
 
-    captioned_image = Image.from_ndarray(captioned_img_np)
+    captioned_img_np = np.array(captioned_img_pil)
+
+    captioned_image = HykoImage.from_ndarray(captioned_img_np)
 
     return Outputs(captioned_image=captioned_image)
