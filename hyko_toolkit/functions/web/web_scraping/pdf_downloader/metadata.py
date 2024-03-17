40ebf96a810f19235fd2f80c12ccc8c8336f0663
ayoubmrx <ak@big-mama.io>
2024-03-17 15:11:00 +0100

    feat: add pdf_downloader tool

---
+++ hyko_toolkit/functions/web/web_scraping/pdf_downloader/metadata.py
@@ -0,0 +1,23 @@
+from hyko_sdk.definitions import ToolkitFunction
+from hyko_sdk.io import PDF
+from hyko_sdk.models import CoreModel
+from pydantic import Field
+
+func = ToolkitFunction(
+    name="pdf_downloader",
+    task="web_scraping",
+    description="This function downloads content from a URL and returns it as a PDF object.",
+)
+
+
+@func.set_input
+class Inputs(CoreModel):
+    url: str = Field(
+        ...,
+        description="Your Target PDF URL.",
+    )
+
+
+@func.set_output
+class Outputs(CoreModel):
+    pdf: PDF = Field(..., description="The Downloaded PDF.")
