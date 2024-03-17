547058c4212cb6f62a28e6143992bd8e3aa237c2
Ouassim Abdelmalek Ghribi <oag@big-mama.io>
2024-03-17 16:29:39 +0100

    style: code formatting

--- hyko_toolkit/functions/utils/image_utils/padding/main.py
+++ hyko_toolkit/functions/utils/image_utils/padding/main.py
@@ -1,8 +1,7 @@
 import numpy as np
+from hyko_sdk.io import Image
 from metadata import Inputs, Outputs, Params, func
 from PIL import Image as PILImage
-
-from hyko_sdk.io import Image
 
 
 @func.on_execute
