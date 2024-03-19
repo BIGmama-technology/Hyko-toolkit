689925f97c7be8913494dcbe4eb5771ae820928f
ayoubmrx <ak@big-mama.io>
2024-03-19 08:48:25 +0100

    refactor: remove empty startup_parms

--- hyko_toolkit/models/optical_character_recognition/surya_ocr/metadata.py
+++ hyko_toolkit/models/optical_character_recognition/surya_ocr/metadata.py
@@ -20,11 +20,6 @@ func = ToolkitModel(
 )
 
 
-@func.set_startup_params
-class StartupParams(CoreModel):
-    pass
-
-
 @func.set_input
 class Inputs(CoreModel):
     image: Image = Field(..., description="Input image.")
