b797c48c3677b936c13d59af994d232358a29465
ayoubmrx <ak@big-mama.io>
2024-03-20 11:35:27 +0100

    feat: add huggingface chat api

---
+++ hyko_toolkit/apis/huggingface/chat/metadata.py
@@ -0,0 +1,84 @@
+from enum import Enum
+
+import httpx
+from hyko_sdk.models import CoreModel, Method
+from pydantic import Field
+
+from hyko_toolkit.apis.api_registry import ToolkitAPI
+from hyko_toolkit.exceptions import APICallError
+
+func = ToolkitAPI(
+    name="huggingface_chat_api",
+    task="huggingface",
+    description="Use huggingface api for text generation.",
+)
+
+
+class Model(str, Enum):
+    NousHermes2Mixtral8x7B_DPO = "NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO"
+    Mixtral8x7B_Instruct_v01 = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+    Mistral7B_Instruct_v02 = "mistralai/Mistral-7B-Instruct-v0.2"
+    zephyr7b_gemma_v01 = "HuggingFaceH4/zephyr-7b-gemma-v0.1"
+    openchat3_5_0106 = "openchat/openchat-3.5-0106"
+    gemma7b_it = "google/gemma-7b-it"
+    gemma_2b = "google/gemma-2b"
+
+
+@func.set_input
+class Inputs(CoreModel):
+    prompt: str = Field(..., description="Input prompt.")
+
+
+@func.set_param
+class Params(CoreModel):
+    model: Model = Field(
+        default=Model.Mistral7B_Instruct_v02,
+        description="Huggingface model to use.",
+    )
+    user_access_token: str = Field(description="API key")
+    max_new_tokens: int = Field(
+        default=1024,
+        description="The maximum number of tokens that can be generated in the chat completion.",
+    )
+    temperature: float = Field(
+        default=1.0,
+        description="What sampling temperature to use, between 0 and 2, defaults to 1.",
+    )
+
+
+@func.set_output
+class Outputs(CoreModel):
+    result: str = Field(..., description="generated text.")
+
+
+class GeneratedText(CoreModel):
+    generated_text: str
+
+
+class MistralResponse(CoreModel):
+    result: list[GeneratedText]
+
+
+@func.on_call
+async def call(inputs: Inputs, params: Params):
+    async with httpx.AsyncClient() as client:
+        res = await client.request(
+            method=Method.post,
+            url=f"https://api-inference.huggingface.co/models/{params.model.value}",
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {params.user_access_token}",
+            },
+            json={
+                "inputs": inputs.prompt,
+                "temperature": params.temperature,
+                "max_new_tokens": params.max_new_tokens,
+            },
+            timeout=60 * 10,
+        )
+    if res.is_success:
+        response = MistralResponse.parse_obj({"result": res.json()})
+    else:
+        raise APICallError(status=res.status_code, detail=res.text)
+
+    return Outputs(result=response.result[0].generated_text)
