f21c64b9c7ea6a925ec8f01c021e28f395321442
ayoubmrx <ak@big-mama.io>
2024-03-21 13:11:22 +0100

    feat: add arxiv articles lookup api

--- hyko_toolkit/apis/__init__.py
+++ hyko_toolkit/apis/__init__.py
@@ -1,6 +1,7 @@
 """register all apis"""
 
 ### Openai API
-from .mediawiki.text_lookup.metadata import func as func
+from .arxiv.articles_lookup.metadata import func as func
+from .mediawiki.text_lookup.metadata import func as func  # noqa: F811
 from .openai.text_completion.metadata import func as func  # noqa: F811
 from .openai.text_embedding.metadata import func as func  # noqa: F811
