63ee5c46df38c9328899a1e02f487bfb418550f7
ayoubmrx <ak@big-mama.io>
2024-03-21 12:19:09 +0100

    feat: add wikipedia text lookup api

--- hyko_toolkit/apis/__init__.py
+++ hyko_toolkit/apis/__init__.py
@@ -1,5 +1,6 @@
 """register all apis"""
 
 ### Openai API
-from .openai.text_completion.metadata import func as func
+from .mediawiki.text_lookup.metadata import func as func
+from .openai.text_completion.metadata import func as func  # noqa: F811
 from .openai.text_embedding.metadata import func as func  # noqa: F811
