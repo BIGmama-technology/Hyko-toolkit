8c9e1283cad4a6630cfc4c48279ae250e8bd64d2
1hachem <hb@big-mama.io>
2024-03-26 13:23:43 +0100

    fix: wikimedia import

--- hyko_toolkit/apis/__init__.py
+++ hyko_toolkit/apis/__init__.py
@@ -1,10 +1,9 @@
 """register all apis"""
 
-### Openai API
 from .arxiv.articles_lookup.metadata import func as func
 from .google.text_lookup.metadata import func as func  # noqa: F811
-from .mediawiki.text_lookup.metadata import func as func  # noqa: F811
 from .openai.text_completion.metadata import func as func  # noqa: F811
 from .openai.text_embedding.metadata import func as func  # noqa: F811
 from .serpapi.bing.metadata import func as func  # noqa: F811
 from .serpapi.duckduckgo.metadata import func as func  # noqa: F811
+from .wikimedia.text_lookup.metadata import func as func  # noqa: F811
