bc6a25971360dcb542297b51b048bf7ec165bee9
ayoubmrx <ak@big-mama.io>
2024-03-26 09:23:01 +0100

    feat: add text_to_image stability_ai api

--- hyko_toolkit/apis/__init__.py
+++ hyko_toolkit/apis/__init__.py
@@ -3,3 +3,4 @@
 ### Openai API
 from .openai.text_completion.metadata import func as func
 from .openai.text_embedding.metadata import func as func  # noqa: F811
+from .stability_ai.text_to_image.metadata import func as func  # noqa: F811
---
+++ hyko_toolkit/apis/stability_ai/text_to_image/metadata.py
@@ -0,0 +1,82 @@
+from enum import Enum
+
+import httpx
+from hyko_sdk.io import Image
+from hyko_sdk.models import CoreModel, Ext, Method
+from pydantic import Field
+
+from hyko_toolkit.apis.api_registry import ToolkitAPI
+from hyko_toolkit.exceptions import APICallError
+
+func = ToolkitAPI(
+    name="text_to_image",
+    task="stability_ai",
+    description="Use Stability.ai API for Image generation.",
+)
+
+
+class AspectRatio(str, Enum):
+    RATIO_16_9 = "16:9"
+    RATIO_1_1 = "1:1"
+    RATIO_21_9 = "21:9"
+    RATIO_2_3 = "2:3"
+    RATIO_3_2 = "3:2"
+    RATIO_4_5 = "4:5"
+    RATIO_5_4 = "5:4"
+    RATIO_9_16 = "9:16"
+    RATIO_9_21 = "9:21"
+
+
+class OutputFormat(str, Enum):
+    webp = "webp"
+    jpeg = "jpeg"
+    png = "png"
+
+
+@func.set_input
+class Inputs(CoreModel):
+    prompt: str = Field(..., description="What you wish to see in the output image.")
+
+
+@func.set_param
+class Params(CoreModel):
+    api_key: str = Field(description="API key")
+    negative_prompt: str = Field(
+        default="", description="What you do not wish to see in the output image.."
+    )
+    output_format: OutputFormat = Field(
+        default=OutputFormat.png, description="Output format"
+    )
+    seed: int = Field(default=0, description="Seed")
+
+
+@func.set_output
+class Outputs(CoreModel):
+    result: Image = Field(..., description="Generated Image.")
+
+
+class Response(CoreModel):
+    image: bytes
+
+
+@func.on_call
+async def call(inputs: Inputs, params: Params):
+    async with httpx.AsyncClient() as client:
+        res = await client.request(
+            method=Method.post,
+            url="https://api.stability.ai/v2beta/stable-image/generate/core",
+            headers={"authorization": f"Bearer {params.api_key}", "accept": "image/*"},
+            files={"none": ""},
+            json={
+                "prompt": inputs.prompt,
+                "negative_prompt": params.negative_prompt,
+                "output_format": params.output_format.value,
+                "seed": params.seed,
+            },
+            timeout=60 * 5,
+        )
+    if res.is_success:
+        response = Response(image=res.content)
+    else:
+        raise APICallError(status=res.status_code, detail=res.text)
+    return Outputs(result=Image(val=response.image, obj_ext=Ext.PNG))
