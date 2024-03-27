425b7b9b46f9a45193f874b19bbfe44a258ec1ac
ayoubmrx <ak@big-mama.io>
2024-03-27 10:06:06 +0100

    feat: add image_to_image_with_prompt api

--- hyko_toolkit/apis/__init__.py
+++ hyko_toolkit/apis/__init__.py
@@ -3,5 +3,6 @@
 ### Openai API
 from .openai.text_completion.metadata import func as func
 from .openai.text_embedding.metadata import func as func  # noqa: F811
+from .stability_ai.image_to_image_with_prompt.metadata import func as func  # noqa: F811
 from .stability_ai.image_upscaler.metadata import func as func  # noqa: F811
 from .stability_ai.text_to_image.metadata import func as func  # noqa: F811
