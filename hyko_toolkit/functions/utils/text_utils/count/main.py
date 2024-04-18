5714ec8e3ac658354bb79c45b1b798e5145a2203
ayoubmrx <ak@big-mama.io>
2024-04-18 12:06:36 +0100

    refactor: remove utils function

--- hyko_toolkit/functions/utils/text_utils/count/main.py
+++
@@ -1,6 +0,0 @@
-from metadata import Inputs, Outputs, Params, func
-
-
-@func.on_execute
-async def main(inputs: Inputs, params: Params) -> Outputs:
-    return Outputs(count=inputs.text.count(params.substring))
--- hyko_toolkit/functions/utils/text_utils/split/main.py
+++
@@ -1,6 +0,0 @@
-from metadata import Inputs, Outputs, Params, func
-
-
-@func.on_execute
-async def main(inputs: Inputs, params: Params) -> Outputs:
-    return Outputs(splitted=inputs.text.split(params.delimiter))
