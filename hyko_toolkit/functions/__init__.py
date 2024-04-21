c2c943501a12c75838c80e9f1049cb268a30e135
1hachem <hb@big-mama.io>
2024-04-21 11:34:09 +0100

    refactor: remove old utils functions

--- hyko_toolkit/functions/__init__.py
+++ hyko_toolkit/functions/__init__.py
@@ -1,5 +1,4 @@
 # Register Functions
-from .converters.image_converter.metadata import func as func  # noqa: F811
 from .converters.markdown_to_pdf.metadata import func as func  # noqa: F811
 from .converters.ocr_pdf_to_text.metadata import func as func  # noqa: F811
 from .converters.pdf_to_text.metadata import func as func  # noqa: F811
@@ -11,57 +10,6 @@ from .downloaders.youtube_downloader.metadata import (  # noqa: F811
 )
 from .plotting.bivariate_plot.metadata import func as func  # noqa: F811
 from .plotting.univariate_plot.metadata import func as func  # noqa: F811
-from .utils.image_utils.brightness_and_contrast.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.image_utils.caption.metadata import func as func  # noqa: F811
-from .utils.image_utils.crop_border.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.image_utils.crop_edges.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.image_utils.flip.metadata import func as func  # noqa: F811
-from .utils.image_utils.get_dimensions.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.image_utils.opacity.metadata import func as func  # noqa: F811
-from .utils.image_utils.padding.metadata import func as func  # noqa: F811
-from .utils.image_utils.resize_factor.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.image_utils.resize_resolution.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.image_utils.rotate.metadata import func as func  # noqa: F811
-from .utils.image_utils.stack_images.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.math_utils.arithmetic.metadata import func as func  # noqa: F811
-from .utils.math_utils.random.metadata import func as func  # noqa: F811
-from .utils.math_utils.range.metadata import func as func  # noqa: F811
-from .utils.math_utils.round.metadata import func as func  # noqa: F811
-from .utils.nlp_utils.recursive_character_text_splitter.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.nlp_utils.remove_special_characters.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.nlp_utils.remove_stopwords.metadata import (
-    func as func,  # noqa: F811
-)
-from .utils.text_utils.concat.metadata import func as func  # noqa: F811
-from .utils.text_utils.count.metadata import func as func  # noqa: F811
-from .utils.text_utils.index_mapping.metadata import (  # noqa: F811
-    func as func,
-)
-from .utils.text_utils.join.metadata import func as func  # noqa: F811
-from .utils.text_utils.length.metadata import func as func  # noqa: F811
-from .utils.text_utils.lowercase.metadata import func as func  # noqa: F811
-from .utils.text_utils.padding.metadata import func as func  # noqa: F811
-from .web.web_search.duckduckgo_search.metadata import (
-    func as func,  # noqa: F811
-)
 from .web.web_search.wikipedia_search.metadata import (
     func as func,  # noqa: F811
 )
