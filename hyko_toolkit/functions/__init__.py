2f50b23eb60b08ea2b22e6ab3468bfc48d186c56
1hachem <hb@big-mama.io>
2024-04-21 13:12:48 +0100

    fix: init some missing functions

--- hyko_toolkit/functions/__init__.py
+++ hyko_toolkit/functions/__init__.py
@@ -1,4 +1,5 @@
 # Register Functions
+
 from .converters.markdown_to_pdf.metadata import func as func  # noqa: F811
 from .converters.ocr_pdf_to_text.metadata import func as func  # noqa: F811
 from .converters.pdf_to_text.metadata import func as func  # noqa: F811
@@ -10,6 +11,18 @@ from .downloaders.youtube_downloader.metadata import (  # noqa: F811
 )
 from .plotting.bivariate_plot.metadata import func as func  # noqa: F811
 from .plotting.univariate_plot.metadata import func as func  # noqa: F811
+from .utils.nlp_utils.recursive_character_text_splitter.metadata import (
+    func as func,  # noqa: F811
+)
+from .utils.nlp_utils.remove_stopwords.metadata import func as func  # noqa: F811
+from .web.web_scraping.beautifulsoup_transformer.metadata import (
+    func as func,  # noqa: F811
+)
+from .web.web_scraping.html2text_transformer.metadata import func as func  # noqa: F811
+from .web.web_scraping.youtube_transcript.metadata import func as func  # noqa: F811
+from .web.web_search.duckduckgo_search.metadata import (
+    func as func,  # noqa: F811
+)
 from .web.web_search.wikipedia_search.metadata import (
     func as func,  # noqa: F811
 )
