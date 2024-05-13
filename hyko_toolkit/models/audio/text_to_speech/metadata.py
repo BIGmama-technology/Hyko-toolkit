2c46064344a6a5d1b0895de35becf70a6d69941b
ayoubmrx <ak@big-mama.io>
2024-05-13 18:34:02 +0100

    refactor: migrate hf audio pipline to new metadata

--- hyko_toolkit/models/audio/text_to_speech/metadata.py
+++ hyko_toolkit/models/audio/text_to_speech/metadata.py
@@ -1,6 +1,7 @@
+from hyko_sdk.components.components import Slider
 from hyko_sdk.io import Audio
 from hyko_sdk.models import CoreModel
-from pydantic import Field
+from hyko_sdk.utils import field
 
 from hyko_toolkit.registry import ToolkitModel
 
@@ -15,28 +16,34 @@ func = ToolkitModel(
 
 @func.set_startup_params
 class StartupParams(CoreModel):
-    hugging_face_model: str = Field(..., description="Model")
-    device_map: str = Field(..., description="Device map (Auto, CPU or GPU)")
+    hugging_face_model: str = field(description="Model")
+    device_map: str = field(description="Device map (Auto, CPU or GPU)")
 
 
 @func.set_input
 class Inputs(CoreModel):
-    text: str = Field(..., description="Input text")
+    text: str = field(description="Input text")
 
 
 @func.set_param
 class Params(CoreModel):
-    top_k: int = Field(
-        default=1, description="Keep best k options (exploration vs. fluency)"
+    top_k: int = field(
+        default=2,
+        description="Number of top predictions to return (default: 2).",
+        component=Slider(leq=0, geq=5, step=1),
     )
-    temperature: float = Field(
-        default=0.5, description="Randomness (fluency vs. creativity)"
+    temperature: float = field(
+        default=0.5,
+        description="Randomness (fluency vs. creativity)",
+        component=Slider(leq=0, geq=1, step=0.01),
     )
-    top_p: float = Field(
-        default=0.5, description="Focus high-probability words (diversity control)"
+    top_p: float = field(
+        default=0.5,
+        description="Focus high-probability words (diversity control)",
+        component=Slider(leq=0, geq=1, step=0.01),
     )
 
 
 @func.set_output
 class Outputs(CoreModel):
-    speech: Audio = Field(..., description="Synthesized speech")
+    speech: Audio = field(description="Synthesized speech")
