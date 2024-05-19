5da32a04d90b06b5b7be33b566845e73d66c0b99
yous92 <jab@big-mama.io>
2024-05-19 13:37:02 +0100

    feat: added image_to_text_search callback

--- hyko_toolkit/models/multimodal/image_to_text/metadata.py
+++ hyko_toolkit/models/multimodal/image_to_text/metadata.py
@@ -1,7 +1,10 @@
+from hyko_sdk.components.components import Search
 from hyko_sdk.io import Image
-from hyko_sdk.models import CoreModel
+from hyko_sdk.models import CoreModel, ModelMetaData
+from hyko_sdk.utils import field
 from pydantic import Field
 
+from hyko_toolkit.callbacks_utils import huggingface_models_search
 from hyko_toolkit.registry import ToolkitModel
 
 func = ToolkitModel(
@@ -15,7 +18,10 @@ func = ToolkitModel(
 
 @func.set_startup_params
 class StartupParams(CoreModel):
-    hugging_face_model: str = Field(..., description="Model")
+    hugging_face_model: str = field(
+        description="Model",
+        component=Search(placeholder="Search image to text model"),
+    )
     device_map: str = Field(..., description="Device map (Auto, CPU or GPU)")
 
 
@@ -43,3 +49,10 @@ class Params(CoreModel):
 @func.set_output
 class Outputs(CoreModel):
     generated_text: str = Field(..., description="Generated text")
+
+
+@func.callback(triggers=["hugging_face_model"], id="image_to_text_search")
+async def add_search_results(
+    metadata: ModelMetaData, access_token: str, refresh_token: str
+) -> ModelMetaData:
+    return await huggingface_models_search(metadata)
