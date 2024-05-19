89e2c1508cace90b782f4889df61ce62f0c5e9cb
yous92 <jab@big-mama.io>
2024-05-19 11:33:34 +0100

    feat: added image_segmentation_search callback

--- hyko_toolkit/models/computer_vision/huggingface/image_segmentation/metadata.py
+++ hyko_toolkit/models/computer_vision/huggingface/image_segmentation/metadata.py
@@ -1,7 +1,10 @@
+from hyko_sdk.components.components import Search
 from hyko_sdk.io import Image
-from hyko_sdk.models import CoreModel
+from hyko_sdk.models import CoreModel, ModelMetaData
+from hyko_sdk.utils import field
 from pydantic import Field
 
+from hyko_toolkit.callbacks_utils import huggingface_models_search
 from hyko_toolkit.registry import ToolkitModel
 
 func = ToolkitModel(
@@ -15,7 +18,10 @@ func = ToolkitModel(
 
 @func.set_startup_params
 class StartupParams(CoreModel):
-    hugging_face_model: str = Field(..., description="Model")
+    hugging_face_model: str = field(
+        description="Model",
+        component=Search(placeholder="Search image segmentation model"),
+    )
     device_map: str = Field(..., description="Device map (Auto, CPU or GPU)")
 
 
@@ -42,3 +48,10 @@ class Params(CoreModel):
 @func.set_output
 class Outputs(CoreModel):
     mask: Image = Field(..., description="Segmented image")
+
+
+@func.callback(triggers=["hugging_face_model"], id="image_segmentation_search")
+async def add_search_results(
+    metadata: ModelMetaData, access_token: str, refresh_token: str
+) -> ModelMetaData:
+    return await huggingface_models_search(metadata)
