ce5daa381ca873b0d5ef6db69f0b0e6a4c3a0b4e
yous92 <jab@big-mama.io>
2024-05-19 12:27:47 +0100

    feat: added video_classification_search callback

--- hyko_toolkit/models/computer_vision/huggingface/video_classification/metadata.py
+++ hyko_toolkit/models/computer_vision/huggingface/video_classification/metadata.py
@@ -1,7 +1,10 @@
+from hyko_sdk.components.components import Search
 from hyko_sdk.io import Video
-from hyko_sdk.models import CoreModel
+from hyko_sdk.models import CoreModel, ModelMetaData
+from hyko_sdk.utils import field
 from pydantic import Field
 
+from hyko_toolkit.callbacks_utils import huggingface_models_search
 from hyko_toolkit.registry import ToolkitModel
 
 func = ToolkitModel(
@@ -15,7 +18,10 @@ func = ToolkitModel(
 
 @func.set_startup_params
 class StartupParams(CoreModel):
-    hugging_face_model: str = Field(..., description="Model")
+    hugging_face_model: str = field(
+        description="Model",
+        component=Search(placeholder="Search video classification model"),
+    )
     device_map: str = Field(..., description="Device map (Auto, CPU or GPU)")
 
 
@@ -39,3 +45,10 @@ class Params(CoreModel):
 class Outputs(CoreModel):
     labels: list[str] = Field(..., description="Class of the video.")
     scores: list[float] = Field(..., description="Scores for each class.")
+
+
+@func.callback(triggers=["hugging_face_model"], id="video_classification_search")
+async def add_search_results(
+    metadata: ModelMetaData, access_token: str, refresh_token: str
+) -> ModelMetaData:
+    return await huggingface_models_search(metadata)
