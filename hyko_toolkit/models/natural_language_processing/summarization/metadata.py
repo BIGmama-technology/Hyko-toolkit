4854eeffdcd1bb5c63e257bc9a2b67a44e2aa9a7
yous92 <jab@big-mama.io>
2024-05-19 13:49:34 +0100

    feat: added summarization callback

--- hyko_toolkit/models/natural_language_processing/summarization/metadata.py
+++ hyko_toolkit/models/natural_language_processing/summarization/metadata.py
@@ -1,6 +1,9 @@
-from hyko_sdk.models import CoreModel
+from hyko_sdk.components.components import Search
+from hyko_sdk.models import CoreModel, ModelMetaData
+from hyko_sdk.utils import field
 from pydantic import Field
 
+from hyko_toolkit.callbacks_utils import huggingface_models_search
 from hyko_toolkit.registry import ToolkitModel
 
 func = ToolkitModel(
@@ -14,7 +17,10 @@ func = ToolkitModel(
 
 @func.set_startup_params
 class StartupParams(CoreModel):
-    hugging_face_model: str = Field(..., description="Model")
+    hugging_face_model: str = field(
+        description="Model",
+        component=Search(placeholder="Search summarization model"),
+    )
     device_map: str = Field(..., description="Device map (Auto, CPU or GPU)")
 
 
@@ -41,3 +47,10 @@ class Params(CoreModel):
 @func.set_output
 class Outputs(CoreModel):
     summary_text: str = Field(..., description="Summary output")
+
+
+@func.callback(triggers=["hugging_face_model"], id="summarization_search")
+async def add_search_results(
+    metadata: ModelMetaData, access_token: str, refresh_token: str
+) -> ModelMetaData:
+    return await huggingface_models_search(metadata)
