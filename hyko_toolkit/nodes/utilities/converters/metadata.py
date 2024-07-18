87dbbcd3c09f6560796ae5bcaeb6b82d406b1877
1hachem <hb@big-mama.io>
2024-07-18 11:45:21 +0100

    refactor: remove video to audio converter since its not working properly

--- hyko_toolkit/nodes/utilities/converters/metadata.py
+++ hyko_toolkit/nodes/utilities/converters/metadata.py
@@ -3,7 +3,6 @@ from hyko_sdk.models import Tag
 from hyko_toolkit.node_group import NodeGroup
 
 from .image_converter.metadata import node as image_converter_node
-from .video_to_audio.metadata import node as video_to_audio_node
 
 node = NodeGroup(
     name="converters",
@@ -12,6 +11,5 @@ node = NodeGroup(
     tag=Tag.utilities,
     nodes=[
         image_converter_node,
-        video_to_audio_node,
     ],
 )
